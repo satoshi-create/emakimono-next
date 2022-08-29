ea02c1a913a09405c6512faaf16ec80038d9c58f
satoshi <mimizunobouken@gmail.com>
2022-08-29 15:41:25 +0900

    EmakiImageコンポーネントを追加
    Former-commit-id: d446576bc926e6c47ca814aa79d74a2e469bf2a3 [formerly ea785b47720886f737ac089fb060ab5354927932]
    Former-commit-id: 61be753550597cf8fdcfbebdc4fdcf3eeef32770

--- pages/emakis/[slug].js
+++ pages/emakis/[slug].js
@@ -3,57 +3,16 @@ import styles from "../../styles/emaki.module.css";
 import "lazysizes";
 import "lazysizes/plugins/attrchange/ls.attrchange";
 import emakiData from "../../libs/data";
+import EmakiImage from "../../components/EmakiImage";
 
 const emaki = () => {
-  console.log(emakiData[[0]].emakis);
   const cyoujyuuKou = emakiData[[0]].emakis;
   return (
     <div className="conteiner">
       {cyoujyuuKou.map((item, index) => {
-        const {
-          cat,
-          kobun,
-          gendaibun,
-          src,
-          name,
-          chapter,
-          srcSp,
-          srcTb,
-          load,
-          viewBoxW,
-          viewBoxH,
-          clickImg,
-        } = item;
-
+        const { cat } = item;
         if (cat == "image") {
-          return (
-            <div className="box" key={index}>
-              <picture>
-                <source
-                  data-srcset={srcSp}
-                  media="(max-height: 375px)"
-                  type="image/webp"
-                />
-                <source
-                  data-srcset={srcTb}
-                  media="(max-height: 800px)"
-                  type="image/webp"
-                />
-                <source data-srcset={src} type="image/webp" />
-                <img
-                  decoding="async"
-                  src={
-                    load
-                      ? srcSp
-                      : "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-                  }
-                  className="loading lazyload"
-                  alt={name}
-                  data-expand="600"
-                />
-              </picture>
-            </div>
-          );
+          return <EmakiImage key={index} item={{...item}} />;
         }
       })}
     </div>
