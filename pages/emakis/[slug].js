ba8178a8277ed593b3d93b1e045aa7e6aff2e4e6
satoshi <mimizunobouken@gmail.com>
2022-08-30 19:09:53 +0900

    ・Sidebarコンポーネントを追加
    Former-commit-id: e12684d3b9b94a30f21fc5f20fed3b6634cb0ad6

--- pages/emakis/[slug].js
+++ pages/emakis/[slug].js
@@ -3,17 +3,22 @@ import "lazysizes";
 import "lazysizes/plugins/attrchange/ls.attrchange";
 import EmakiConteiner from "../../components/EmakiConteiner";
 import emakisData from "../../libs/data";
+import Sidebar from "../../components/Sidebar";
 
 const Emaki = () => {
   const router = useRouter();
   const { slug } = router.query;
   console.log(slug);
 
-  const filterdEmakisData = emakisData.find((item, index) => item.titleen === slug)
-  
-  console.log({...filterdEmakisData});
+  const filterdEmakisData = emakisData.find(
+    (item, index) => item.titleen === slug
+  );
 
-  return <EmakiConteiner data={{...filterdEmakisData}} />;
+  console.log({ ...filterdEmakisData });
+
+  return (
+      <EmakiConteiner data={{ ...filterdEmakisData }} />
+  );
 };
 
 export default Emaki;
