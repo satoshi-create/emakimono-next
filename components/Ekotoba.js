e915e11f988b190dcaa567776cc3ed09065ff05d
satoshi <mimizunobouken@gmail.com>
2022-08-31 17:38:37 +0900

    Ekotoba.js
    ・クリックすると古文を表示するボタンを追加
    
    
    Former-commit-id: 52c123f8684ccca49cf3b0a3c7b9dffb21dcfa21 [formerly 19ff8cbcec88f23a8157766927a5b3dc6e8cb5c3]
    Former-commit-id: 136d69856d9f1f54c40488cdb2cf3576ccf1726e

--- components/Ekotoba.js
+++ components/Ekotoba.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Ekotoba.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import ResposiveImage from "./ResposiveImage";
 
 const Ekotoba = ({
-  item: { chapter, gendaibun, kobun, index, backgroundImage },
+  item: {
+    chapter,
+    gendaibun,
+    kobun,
+    index,
+    backgroundImage,
+    srcSp,
+    srcTb,
+    src,
+    load,
+    name,
+  },
 }) => {
+  const [toggle, setToggle] = useState(false);
   return (
     <section
       className={styles.ekotoba}
@@ -13,10 +28,26 @@ const Ekotoba = ({
       <div className={styles.gendaibun}>
         <h3 dangerouslySetInnerHTML={{ __html: chapter }} />
         <p dangerouslySetInnerHTML={{ __html: gendaibun }} />
+        <button className={styles.togglebtn}>
+          <i>
+            <FontAwesomeIcon
+              icon={toggle ? faMinus : faPlus}
+              title={toggle ? "閉じる" : "詞書の現代語訳と原文を比べて読む"}
+              onClick={() => setToggle(!toggle)}
+            />
+          </i>
+        </button>
       </div>
-      <div className={styles.kobun}>
+      <div
+        className={
+          toggle ? `${styles.kobun} ${styles.open}` : `${styles.kobun}`
+        }
+      >
         <p dangerouslySetInnerHTML={{ __html: kobun }} />
       </div>
+      <span className={styles.calligraphy}>
+        <ResposiveImage value={{ srcSp, srcTb, src, load, name }} />
+      </span>
     </section>
   );
 };
