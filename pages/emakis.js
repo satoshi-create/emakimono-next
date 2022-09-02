ad2d2488f259adc6be08cded35eae794ea05bec6
satoshi <mimizunobouken@gmail.com>
2022-09-02 10:38:50 +0900

    ・emakisページにスタイルを追加
    Former-commit-id: d231f1bc75f56c17971183c9fd6400b5eecfb884 [formerly 2a6af3c53ce0128bdb4e060c341e57d5a40b4aad]
    Former-commit-id: 6fc416c4f306bd67def10fc2806bf4fc7c5731e4

--- pages/emakis.js
+++ pages/emakis.js
@@ -1,20 +1,55 @@
 import "lazysizes";
 import emakisData from "../libs/data";
 import Link from "next/link";
+import ResposiveImage from "../components/ResposiveImage";
+import styles from "../styles/Emakis.module.css";
 
 export default function emakis() {
   return (
-    <div className="conteinter">
+    <section className={`section-center ${styles.conteiner}`}>
       {emakisData.map((item, index) => {
-        const { titleen, title } = item;
+        const {
+          titleen,
+          title,
+          thumb,
+          edition,
+          author,
+          era,
+          desc,
+          type,
+          typeColor,
+        } = item;
         return (
-          <div className="emaki-card" key={index}>
+          <div className={styles.card} key={index}>
             <Link href={`/emakis/${titleen}`}>
-              <a>{title}</a>
+              <div className={styles.single}>
+                <picture>
+                  <source data-srcset={thumb} type="image/webp" />
+                  <img
+                    src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                    className="lazyload loading"
+                    alt={thumb}
+                  />
+                </picture>
+                <div className={`${typeColor} ${styles.type}`}>{type}</div>
+              </div>
             </Link>
+            <div className={styles.info}>
+              <div className={styles.header}>
+                <h3>
+                  {title}
+                  {edition ? edition : ""}
+                </h3>
+                <h4 className={styles.author}>{author}</h4>
+                <h4 className={styles.era}>{era}</h4>
+              </div>
+              <div className={styles.desc}>
+                <p>{desc}</p>
+              </div>
+            </div>
           </div>
         );
       })}
-    </div>
+    </section>
   );
 }
