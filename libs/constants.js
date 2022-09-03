f77fa7f429bffed85c94bebd181d1f6ae5798cea
satoshi <mimizunobouken@gmail.com>
2022-09-03 15:33:30 +0900

    fixed
    Former-commit-id: b66a2bfd3d07f42e65510d0a1f537871bc9e533e [formerly fe594bd1ec9a734da28e673d9db99d494df73de8]
    Former-commit-id: 5a9af9cb2d2a275a0dff09c36173fbb4d455f41c

--- libs/constants.js
+++ libs/constants.js
@@ -2,7 +2,7 @@ const siteMeta = {
   siteTitle: "横スクロールで楽しむ絵巻物",
   siteDesc:
     "縦書き、横スクロールで、絵巻物本来の見方を楽しむことを追求しているサイトです。本サイトで、絵巻物を繰りひろげながら絵を追いかける面白さを、体験してみてください",
-  siteUrl: "https://emakimono-next.vercel.app/",
+  siteUrl: "https://emakimono-next.vercel.app",
   siteLang: "ja",
   siteLocale: "ja_JP",
   siteType: "website",
