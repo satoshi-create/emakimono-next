f8887c5b2e8881acf9ebe7b211570ec5fc0d3bbe
satoshi <mimizunobouken@gmail.com>
2022-09-03 14:46:40 +0900

    ・Metaコンポーネントを追加 ・メタデータを作成

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Head from "../components/Meta";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const about = () => {
   return (
-    <div>about</div>
-  )
-}
+    <>
+      <Head pagetitle={"アバウト"} pageDesc={"アバウトページです"} />
+      <Header />
+      <div>製作中です</div>
+      {/* <Footer /> */}
+    </>
+  );
+};
 
-export default about
+export default about;
