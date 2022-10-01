a3fc5b236cebd7cba5535b427ef690f1b226972c
satoshi <mimizunobouken@gmail.com>
2022-10-01 17:23:15 +0900

    ・屏風ページを追加
    Former-commit-id: e2bda6f1f1de252156445dd6da2bbe9ca4d8c0bb [formerly e6e0ae8efd38a800a1bb9932353480dc25e90cb7]
    Former-commit-id: 3108130a7537e746f57fb3e2b1cd26bec8fc6730

--- libs/links.js
+++ libs/links.js
@@ -1,7 +1,8 @@
 const links = [
   { name: "Home", path: "/" },
-  { name: "About", path: "/about" },
-  { name: "絵巻図鑑", path: "/emakis" },
+  // { name: "About", path: "/about" },
+  { name: "絵巻", path: "/emakis" },
+  { name: "屏風", path: "/byoubu" },
 ];
 
 export default links;
