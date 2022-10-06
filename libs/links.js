227e5624fc9bb91686affe37f04a800eac949164
satoshi <mimizunobouken@gmail.com>
2022-10-06 12:39:04 +0900

    fixed
    Former-commit-id: 851e7b8e06c6b6e4f60457dcba47be4b5bc815c7 [formerly d95455d61f4fd8f93416c312bb0fe02089fa5641]
    Former-commit-id: ad1a9d64f7fe71b61f15f95975ecfed82382b159

--- libs/links.js
+++ libs/links.js
@@ -3,6 +3,7 @@ const links = [
   // { name: "About", path: "/about" },
   { name: "絵巻", path: "/emakis" },
   { name: "屏風", path: "/byoubu" },
+  { name: "西洋絵画", path: "/seiyoukaiga" },
 ];
 
 export default links;
