9c599157ce5cb585c08fc97ee9c06b99d795daf4
satoshi <mimizunobouken@gmail.com>
2022-10-14 19:13:46 +0900

    ・「千絵の海」（葛飾北斎）を追加
    Former-commit-id: 81a31282ffb94e91fd5d691c27a12d19281d1fca [formerly fdfac0a8368ab63406159f1eca672b8fd247ffc6]
    Former-commit-id: 2edb3b399fd0f78b36bbfd3d64c028074b56b62b

---
+++ pages/ukiyoe.js
@@ -0,0 +1,27 @@
+import "lazysizes";
+import data from "../libs/data";
+import Link from "next/link";
+import ResposiveImage from "../components/ResposiveImage";
+import styles from "../styles/Emakis.module.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import CardConteiner from "../components/CardConteiner";
+import Head from "../components/Meta";
+import Title from "../components/Title";
+import SerchForm from "../components/SerchForm";
+
+export default function emakis() {
+  const emakisData = data.filter((item) => item.type.includes("浮世絵"));
+  console.log(emakisData);
+  return (
+    <>
+      <Head pagetitle={"浮世絵"} pageDesc={"浮世絵図鑑のページです"} />
+      <Header />
+      <Title pagetitle={"浮世絵"} />
+      <SerchForm emakis={emakisData} />
+      <CardConteiner emakis={emakisData} />
+      {/* <Footer /> */}
+    </>
+  );
+}
