636d36cdf13c782ec928abe519856be7ac7a3d0a
satoshi <mimizunobouken@gmail.com>
2022-10-31 15:08:43 +0900

    サムネイル画像を追加
    Former-commit-id: 49f0692b4c5ffe30fb758a3816e4aa2ed1e752bb [formerly 199062e50622696c85c96f2619f6e62489b53830]
    Former-commit-id: a3148a16638b57420a86a7492c1797689ed949ab

--- libs/constants.js
+++ libs/constants.js
@@ -1,7 +1,7 @@
 const siteMeta = {
   siteTitle: "横スクロールで楽しむ絵巻物",
   siteDesc:
-    "縦書き、横スクロールで、絵巻物本来の見方を楽しむことを追求しているサイトです。本サイトで、絵巻物を繰りひろげながら絵を追いかける面白さを、体験してみてください",
+    "縦書き、横スクロールで、絵巻物本来の見方を楽しむことを追求しているサイトです。",
   siteUrl: "https://www.emakimono.com/",
   siteLang: "ja",
   siteLocale: "ja_JP",
