4e958768fec43d63dcc783537ce1ab9e9c322739
satoshi <mimizunobouken@gmail.com>
2022-11-08 12:26:30 +0900

    時代区分の動的ページを追加
    Former-commit-id: 53fdd4504738f83233237c1e57ce12bf807c734f

---
+++ libs/era.js
@@ -0,0 +1,10 @@
+const era = [
+  { name: "平安時代", id: "heiann", slug: "heiann" },
+  { name: "鎌倉時代", id: "kamakura", slug: "kamakura" },
+  { name: "室町時代", id: "muromachi,", slug: "muromachi" },
+  { name: "安土・桃山時代", id: "aduchimomoyama", slug: "aduchimomoyama" },
+  { name: "江戸時代", id: "edo", slug: "edo" },
+  { name: "明治時代", id: "meiji", slug: "meiji" },
+];
+
+export default era;
