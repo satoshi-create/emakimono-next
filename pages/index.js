f38c2cdbc60f62ec27083c0524438efc71507e13
satoshi <mimizunobouken@gmail.com>
2022-11-11 13:16:31 +0900

    Cardコンポーネントのスタイルを変更
    Former-commit-id: dfe08aa17149b7fcb4dc09f5c3d8b83fd990b6b8 [formerly df97b5ae9ed29ad3ab16d70aa8e334d535c1374e]
    Former-commit-id: 13128e876fa7278b253cb271f9b2d9fa13b617ab

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,71 @@ import Header from "../components/Header";
 import Card from "../components/Card";
 import emakisData from "../libs/data";
 import "lazysizes";
-import CardConteiner from "../components/CardConteiner";
 import Head from "../components/Meta";
-import SerchForm from "../components/SerchForm";
-import SortType from "../components/SortType";
 
 const index = () => {
+  const favoriteEmakis = emakisData.filter((emaki) => emaki.favorite === true);
+
+  const setsuwaEmakis = emakisData.filter((emaki) => emaki.subtype === "説話");
+  const kousoudenEmakis = emakisData.filter(
+    (emaki) => emaki.subtype === "高僧伝"
+  );
+  const buttenEmakis = emakisData.filter((emaki) => emaki.subtype === "仏典");
+  const gyouziEmakis = emakisData.filter(
+    (emaki) => emaki.subtype === "諸行事・祭礼"
+  );
+
+  const variation = [
+    ...setsuwaEmakis,
+    ...kousoudenEmakis,
+    ...buttenEmakis,
+    ...gyouziEmakis,
+  ];
+
+  console.log(variation);
+
+  const typeByoubu = emakisData
+    .filter((emaki) => emaki.type === "屏風")
+    .splice(0, 1);
+  const typeUkiyoe = emakisData
+    .filter((emaki) => emaki.type === "浮世絵")
+    .splice(0, 1);
+  const typeSuibokuga = emakisData
+    .filter((emaki) => emaki.type === "水墨画")
+    .splice(0, 1);
+  const typeSeiyoukaiga = emakisData
+    .filter((emaki) => emaki.type === "西洋絵画")
+    .splice(0, 1);
+
+  const alpha = [
+    ...typeByoubu,
+    ...typeUkiyoe,
+    ...typeSuibokuga,
+    ...typeSeiyoukaiga,
+  ];
+
   return (
     <>
       <Head />
       <Header />
-      {/* <SerchForm emakis={emakisData} /> */}
-      <SortType emakis={emakisData} />
-      {/* <CardConteiner emakis={emakisData} /> */}
+      <Card
+        emakis={favoriteEmakis}
+        columns={"three"}
+        pagetitle={"三大絵巻"}
+        sectionname={"favorite"}
+      />
+      <Card
+        emakis={variation}
+        columns={"four"}
+        pagetitle={"さまざまな絵巻"}
+        sectionname={"variation"}
+      />
+      <Card
+        emakis={alpha}
+        columns={"two"}
+        pagetitle={"＋α"}
+        sectionname={"alpha"}
+      />
       <Footer />
     </>
   );
