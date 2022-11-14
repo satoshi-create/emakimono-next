ec48cb36f2891e08d2555dc17515975d1f474799
satoshi <mimizunobouken@gmail.com>
2022-11-14 16:20:26 +0900

    fixed
    Former-commit-id: 310e1951c5e4495882fb042a0060794c51accbc0

--- components/EmakiConteiner.js
+++ components/EmakiConteiner.js
@@ -1,14 +1,37 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "lazysizes";
 import "lazysizes/plugins/attrchange/ls.attrchange";
 import emakiData from "../libs/data";
 import EmakiImage from "./EmakiImage";
 import Ekotoba from "./Ekotoba";
 import styles from "../styles/EmakiConteiner.module.css";
+import { faScrollTorah } from "@fortawesome/free-solid-svg-icons";
 
 const EmakiConteiner = ({ data }) => {
-  const { backgroundImage, kotobagaki,type } = data;
+  // const [loading, setLoading] = useState(false);
+  // const [scrollWidth, setScrollWidth] = useState(1000);
+  // const [emakis, setEmakis] = useState([]);
+
   const emakis = data.emakis;
+  const { backgroundImage, kotobagaki, type } = emakis;
+
+  // const fetchImages = async () => {
+  //   setLoading(true);
+  //   setEmakis(data.emakis);
+  //   console.log(emakis);
+  //   setLoading(false);
+  // };
+
+  // useEffect(() => {
+  //   fetchImages();
+  // }, []);
+
+  // useEffect(() => {
+  //   if (!loading) {
+  //     const xwidth = window.outerWidth;
+  //     setScrollWidth((scrollWidth) => scrollWidth + xwidth);
+  //   }
+  // }, []);
 
   const scrollRef = useRef();
 
@@ -29,7 +52,12 @@ const EmakiConteiner = ({ data }) => {
 
   return (
     <>
-      <article className={`${styles.conteiner } ${type === "西洋絵画" ? styles.lr :styles.rl}`} ref={scrollRef}>
+      <article
+        className={`${styles.conteiner} ${
+          type === "西洋絵画" ? styles.lr : styles.rl
+        }`}
+        ref={scrollRef}
+      >
         {emakis.map((item, index) => {
           const { cat } = item;
           if (cat === "image") {
@@ -38,7 +66,7 @@ const EmakiConteiner = ({ data }) => {
             return (
               <Ekotoba
                 key={index}
-                item={{ ...item, index, backgroundImage, kotobagaki ,type}}
+                item={{ ...item, index, backgroundImage, kotobagaki, type }}
               />
             );
           }
