0eb0ecb9d472855149b25791d63e4644de871f60
satoshi <mimizunobouken@gmail.com>
2022-11-17 16:20:05 +0900

    fixed

--- components/Keywords.js
+++ components/Keywords.js
@@ -3,34 +3,15 @@ import Title from "./Title";
 import emakisData from "../libs/data";
 import Link from "next/link";
 import styles from "../styles/Tags.module.css";
+import { keywordItem, personnameItem } from "../libs/func";
 
 const Keywords = ({ sectiontitle, sectiontitleen, allTags, path }) => {
-  const convert = (arr) => {
-    const res = {};
-    arr.forEach((obj) => {
-      const key = `${obj.name}`;
-      if (!res[key]) {
-        res[key] = { ...obj, total: 0 };
-      }
-      res[key].total += 1;
-    });
-    return Object.values(res);
-  };
-
-  const keywordItem = convert(
-    emakisData.flatMap((item) => item.keyword).filter((item) => item)
-  ).sort((a, b) => (a.total > b.total ? -1 : 1));
-
-  const personnameItem = convert(
-    emakisData.flatMap((item) => item.personname).filter((item) => item)
-  ).sort((a, b) => (a.total > b.total ? -1 : 1));
-  console.log(personnameItem);
 
   if (sectiontitle === "キーワード") {
     return (
       <section className={`section-center section-padding ${styles.container}`}>
         <section className={styles.tags}>
-          {keywordItem.map((item, index) => {
+          {keywordItem(emakisData).map((item, index) => {
             const { name, id, slug, total } = item;
 
             return (
@@ -49,7 +30,7 @@ const Keywords = ({ sectiontitle, sectiontitleen, allTags, path }) => {
     return (
       <section className={`section-center section-padding ${styles.container}`}>
         <section className={styles.tags}>
-          {personnameItem.map((item, index) => {
+          {personnameItem(emakisData).map((item, index) => {
             const { name, id, slug, total } = item;
 
             return (
