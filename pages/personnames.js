a9f520f780c251534504f1a4df59288bce5fe011
satoshi <mimizunobouken@gmail.com>
2022-11-23 13:32:14 +0900

    fixed

--- pages/personnames.js
+++ pages/personnames.js
@@ -5,9 +5,12 @@ import "lazysizes";
 import Head from "../components/Meta";
 import Keywords from "../components/Keywords";
 import Breadcrumbs from "../components/Breadcrumbs";
-import allPersonNames from "../libs/allPersonNames";
+import emakisData from "../libs/data";
+import { personnameItem } from "../libs/func";
 
 const personnames = () => {
+  const allPersonNames = personnameItem(emakisData);
+  console.log(allPersonNames);
   return (
     <>
       <Head pagetitle={"人物名一覧"} pageDesc={`人物名一覧のページです`} />
