245f60c282d7ac330c1c5d868c8d8a18c9007bc0
satoshi <mimizunobouken@gmail.com>
2022-11-29 00:17:45 +0900

    fixed
    Former-commit-id: a4a5274b6aceece47bc7d43907de07dfabd2905f [formerly 5a70d7c6689929e6d5870e82ad7970dde950cf2f]
    Former-commit-id: 37ab0253a24cab6ffb2a3c8703a87baa7eeb8333

--- pages/server-sitemap.xml.js
+++ pages/server-sitemap.xml.js
@@ -1,13 +1,14 @@
 import { getServerSideSitemap } from "next-sitemap";
-import siteMeta from "../libs/dataSiteMeta";
+import { jaMeta } from "../libs/dataSiteMeta";
 import emakisData from "../libs/data";
 
 export default function Sitemap() {}
 
 export async function getServerSideProps(context) {
+
   const postFields = emakisData.map((post) => {
     return {
-      loc: `${siteMeta.siteUrl}/${post.typeen}/${post.titleen}`,
+      loc: `${jaMeta.siteUrl}/${post.typeen}/${post.titleen}`,
     };
   });
 
