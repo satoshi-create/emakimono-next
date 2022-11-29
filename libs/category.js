2096184b38c6f612e6da38748d5549e3e0b65bed
satoshi <mimizunobouken@gmail.com>
2022-11-29 13:33:01 +0900

    fixed
    Former-commit-id: 3b254639cba2f567de34ab373ff64b74fc71d5ec [formerly db1cdabb8ff544318993bddbf7d8b83d66fa661f]
    Former-commit-id: 05542eb343ca18ec4f5f845ead646885fae0ca38

--- libs/category.js
+++ libs/category.js
@@ -3,7 +3,7 @@ const category = [
   { name: "屏風", id: "byoubu", slug: "byoubu" },
   { name: "水墨画", id: "suibokuga", slug: "suibokuga" },
   { name: "浮世絵", id: "ukiyoe", slug: "ukiyoe" },
-  { name: "西洋絵画", id: "seiyoukaiga", slug: "seiyoukaiga" },
+  { name: "西洋絵画", id: "Western painting", slug: "seiyoukaiga" },
 ];
 
 export default category;
