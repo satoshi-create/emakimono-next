f592ad4fba7fce99a38ce69e0e6febc063b62537
satoshi <mimizunobouken@gmail.com>
2022-12-03 15:25:12 +0900

    fixed
    Former-commit-id: 4a884b28d93ef1a81576f700cd083a97b0f2e27d

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,14 +2,14 @@ const config = {
   siteUrl: `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}`,
   generateRobotsTxt: true,
   outDir: "./out",
-  transform: async (config, path) => {
-    return {
-      loc: path,
-      changefreq: "weekly",
-      priority: 1,
-      lastmod: new Data().toString(),
-    };
-  },
+  // transform: async (config, path) => {
+  //   return {
+  //     loc: path,
+  //     changefreq: "weekly",
+  //     priority: 1,
+  //     lastmod: new Data().toString(),
+  //   };
+  // },
 };
 
 module.exports = config;
