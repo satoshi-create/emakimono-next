aeca6b7b429ebdee0b70f12167096be8b64e206e
satoshi <mimizunobouken@gmail.com>
2022-12-03 15:22:42 +0900

    fixed
    Former-commit-id: fdf5c4d523e2c600bca393e5538681754b52998b [formerly 4fbf4b1e38088551a00f4913ead66e9de641f462]
    Former-commit-id: c7c9acaa38c39c57520d19ae974d10fa1767c16f

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,15 @@
 const config = {
-  siteUrl: "https://emakimono.com/",
-  // exclude: [`/server-sitemap.xml`],
-  // robotsTxtOptions: {
-  //   additionalSitemaps: ["https://emakimono.com/server-sitemap.xml"],
-  // },
+  siteUrl: `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}`,
+  generateRobotsTxt: true,
+  outDir: "./out",
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: "weekly",
+      priority: 1,
+      lastmod: new Data().toString(),
+    };
+  },
 };
 
 module.exports = config;
