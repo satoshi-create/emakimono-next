4f02393c7f32f70af918401c4a7542494b65c60e
satoshi <mimizunobouken@gmail.com>
2022-12-07 12:20:30 +0900

    ・NavLinksコンポーネントを追加 ・Footerコンポーネントにnavを追加
    Former-commit-id: b6250a3653822ef05911c50293cf073b0b5bb704 [formerly f0b5db0edd9bd57a0ef250c3c7863f29cdfbd749]
    Former-commit-id: 4062e670afa1fee2b217defc7adf4f3ee6ec4632

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "../styles/Footer.module.css";
 import { useRouter } from "next/router";
+import NavLinks from "./NavLinks";
 
 const Footer = () => {
+  const footerstyle = {
+    color: "white",
+    "font-family": "var(--title-font)",
+  };
   const { locale } = useRouter();
   return (
     <footer className={`parts-grid ${styles.footer}`}>
       <div className={styles.center}>
+        <NavLinks footerstyle={footerstyle} />
         <div className={styles.title}>
           {/* <img src="/logo-footer.png" alt="logo" className={styles.logo} /> */}
           <h4>
