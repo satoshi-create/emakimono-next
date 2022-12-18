56c2be07ca301627cb5b722f3d58af16c7c01373
satoshi <mimizunobouken@gmail.com>
2022-12-18 14:45:39 +0900

    ・デフォルトのフォントを追加
    Former-commit-id: f7c3f1bd82a803c58933e21ea06fe12fecab0faf

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
-          />
+          /> */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300;400;500&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
