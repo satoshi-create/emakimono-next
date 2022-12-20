09068712f31320d09e48cec9a5ee1e0c10bc00fa
satoshi <mimizunobouken@gmail.com>
2022-12-20 11:25:43 +0900

    ・一覧ページにボタンがでてくるバグを修正
    Former-commit-id: 585c9e69e843b5b3cb03ef2ca826335665dc8984 [formerly 34bb476dd1350db9ae35a190dd4d838e7235ff07]
    Former-commit-id: 8a53904c0c717e2168a0f2fc4b724ca5b36167f0

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.method);
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      res.json({ message: "GETリクエスト" });
+      break;
+    case "POST":
+      res.json({ message: "POST" });
+    case "PATCH":
+      res.json({ messsage: "PATCH" });
+    default:
+      res.json({ messsage: "enything else" });
+      break;
+  }
+  // res.status(200).json({ name: "John Doe" });
 }
