98cfbebe0ae73847751dd502300348b190bfc927
satoshi <mimizunobouken@gmail.com>
2023-01-13 12:36:35 +0900

    fixed
    Former-commit-id: b6341178d56a44ba37f7a751963785a7546c3b1d [formerly a286a0d20105777abd7163a10a951f4d9ed44eac]
    Former-commit-id: 51a4b03520c735a28d79f7bf048092fbe9a821aa

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "lazysizes";
 import Head from "../components/Meta";
-import styles from "../styles/Contact.module.css";
+import styles from "../styles/contact.module.css";
 import { FormControl, Textarea, Input } from "@chakra-ui/react";
 import Title from "../components/Title";
 
