227ed7d0ab7d7b29a55274c578576284dc029999
satoshi <mimizunobouken@gmail.com>
2023-01-14 11:55:25 +0900

    fixed

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,19 +1,73 @@
+// import { createTransport } from "nodemailer";
+
+// export default async function sendMail(req, res) {
+//   const transporter = createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: process.env.NEXT_PUBLIC_MAIL_USER,
+//       pass: process.env.NEXT_PUBLIC_MAIL_PASS,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from:process.env.NEXT_PUBLIC_MAIL_USER,
+//     to:process.env.NEXT_PUBLIC_MAIL_USER,
+//     subject: `【お問い合わせ】${req.body.name}様より`,
+//     text: req.body.message + " | Sent from: " + req.body.email,
+//     html: `
+//       <p>【名前】</p>
+//       <p>${req.body.name}</p>
+//       <p>【メールアドレス】</p>
+//       <p>${req.body.email}</p>
+//       <p>【メッセージ】</p>
+//       <p>${req.body.message.replaceAll("\n", "<br>")}</p>
+//     `,
+//   });
+
+//   res.status(200).json({
+//     success: true,
+//   });
+// }
+
+
+//   await transporter.sendMail({
+//     from: process.env.MAIL_USER,
+//     to: `${req.body.email}`,
+//     subject: `【お問い合わせ自動受付メール】`,
+//     text: req.body.message + " | Sent from: " + req.body.email,
+//     html: `
+//           <p>
+//             お問い合わせありがとうございます。
+//             <br>以下の内容でお問い合わせを承りました。
+//           </p>
+//           <p>-----------------------------------------</p>
+//             <p>【名前】</p>
+//             <p>${req.body.name}</p>
+//             <p>【メールアドレス】</p>
+//             <p>${req.body.email}</p>
+//             <p>【メッセージ】</p>
+//             <p>${req.body.message.replaceAll("\n", "<br>")}</p>
+//           <p>-----------------------------------------</p>
+//         `,
+//   });
+
 import { createTransport } from "nodemailer";
 
 export default async function sendMail(req, res) {
   const transporter = createTransport({
-    host: "smtp.gmail.com",
+    service: "gmail",
     port: 465,
     secure: true,
     auth: {
-      user: process.env.NEXT_PUBLIC_MAIL_USER,
-      pass: process.env.NEXT_PUBLIC_MAIL_PASS,
+      user: "mimizunobouken@gmail.com",
+      pass: "fzaauhcvhfdtklxc",
     },
   });
-
   await transporter.sendMail({
-    from:process.env.NEXT_PUBLIC_MAIL_USER,
-    to:process.env.NEXT_PUBLIC_MAIL_USER,
+    from: "mimizunobouken@gmail.com",
+    to: "mimizunobouken@gmail.com",
     subject: `【お問い合わせ】${req.body.name}様より`,
     text: req.body.message + " | Sent from: " + req.body.email,
     html: `
@@ -30,25 +84,3 @@ export default async function sendMail(req, res) {
     success: true,
   });
 }
-
-
-  // await transporter.sendMail({
-  //   from: process.env.MAIL_USER,
-  //   to: `${req.body.email}`,
-  //   subject: `【お問い合わせ自動受付メール】`,
-  //   text: req.body.message + " | Sent from: " + req.body.email,
-  //   html: `
-  //         <p>
-  //           お問い合わせありがとうございます。
-  //           <br>以下の内容でお問い合わせを承りました。
-  //         </p>
-  //         <p>-----------------------------------------</p>
-  //           <p>【名前】</p>
-  //           <p>${req.body.name}</p>
-  //           <p>【メールアドレス】</p>
-  //           <p>${req.body.email}</p>
-  //           <p>【メッセージ】</p>
-  //           <p>${req.body.message.replaceAll("\n", "<br>")}</p>
-  //         <p>-----------------------------------------</p>
-  //       `,
-  // });
