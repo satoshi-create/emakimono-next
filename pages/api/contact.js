a02cc326669d180d0691c800bfab00136e37dc61
satoshi <mimizunobouken@gmail.com>
2023-01-14 10:49:53 +0900

    fixed
    Former-commit-id: 76876b3eacc653470f46c53c13fe87f5917d954c

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,80 +1,19 @@
-// export default function sendmail(req, res) {
-//   let nodemailer = require("nodemailer");
-
-//   const transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 465,
-//     secure: true,
-//     auth: {
-//       user: process.env.MAIL_USER,
-//       pass: process.env.MAIL_PASS,
-//     },
-//   });
-
-//   const toHostMailData = {
-//     from: process.env.MAIL_USER,
-//     to: process.env.MAIL_USER,
-//     subject: `【お問い合わせ】${req.body.name}様より`,
-//     text: req.body.message + " | Sent from: " + req.body.email,
-//     html: `
-//           <p>【名前】</p>
-//           <p>${req.body.name}</p>
-//           <p>【メールアドレス】</p>
-//           <p>${req.body.email}</p>
-//           <p>【メッセージ】</p>
-//           <p>${req.body.message.replaceAll("\n", "<br>")}</p>
-//         `,
-//   };
-
-//   const toGuestMailData = {
-//     from: process.env.MAIL_USER,
-//     to: `${req.body.email}`,
-//     subject: `【お問い合わせ自動受付メール】`,
-//     text: req.body.message + " | Sent from: " + req.body.email,
-//     html: `
-//       <p>
-//         お問い合わせありがとうございます。
-//         <br>以下の内容でお問い合わせを承りました。
-//       </p>
-//       <p>-----------------------------------------</p>
-//         <p>【名前】</p>
-//         <p>${req.body.name}</p>
-//         <p>【メールアドレス】</p>
-//         <p>${req.body.email}</p>
-//         <p>【メッセージ】</p>
-//         <p>${req.body.message.replaceAll("\n", "<br>")}</p>
-//       <p>-----------------------------------------</p>
-//     `,
-//   };
-
-//   transporter.sendMail(toHostMailData, function (err, info) {
-//     if (err) console.log(err);
-//     else console.log(info);
-//   });
-
-//   transporter.sendMail(toGuestMailData, function (err, info) {
-//     if (err) console.log(err);
-//     else console.log(info);
-//   });
-
-//   res.send("success");
-// }
-
 import { createTransport } from "nodemailer";
 
 export default async function sendMail(req, res) {
   const transporter = createTransport({
-    service: "gmail",
-    // port: 465,
-    // secure: true,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-      user: "mimizunobouken@gmail.com",
-      pass: "fzaauhcvhfdtklxc",
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
     },
   });
+
   await transporter.sendMail({
-    from: "mimizunobouken@gmail.com",
-    to: "mimizunobouken@gmail.com",
+    from:process.env.MAIL_USER,
+    to:process.env.MAIL_USER,
     subject: `【お問い合わせ】${req.body.name}様より`,
     text: req.body.message + " | Sent from: " + req.body.email,
     html: `
@@ -85,6 +24,27 @@ export default async function sendMail(req, res) {
       <p>【メッセージ】</p>
       <p>${req.body.message.replaceAll("\n", "<br>")}</p>
     `,
+  });
+
+  await transporter.sendMail({
+    from: process.env.MAIL_USER,
+    to: `${req.body.email}`,
+    subject: `【お問い合わせ自動受付メール】`,
+    text: req.body.message + " | Sent from: " + req.body.email,
+    html: `
+          <p>
+            お問い合わせありがとうございます。
+            <br>以下の内容でお問い合わせを承りました。
+          </p>
+          <p>-----------------------------------------</p>
+            <p>【名前】</p>
+            <p>${req.body.name}</p>
+            <p>【メールアドレス】</p>
+            <p>${req.body.email}</p>
+            <p>【メッセージ】</p>
+            <p>${req.body.message.replaceAll("\n", "<br>")}</p>
+          <p>-----------------------------------------</p>
+        `,
   });
 
   res.status(200).json({
