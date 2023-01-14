4f29bb6fbe441ddc79deb6732bd6438ec6bb2961
satoshi <mimizunobouken@gmail.com>
2023-01-14 11:04:26 +0900

    fixed
    Former-commit-id: f7cc9a8d247aee7520f15794205174d6ea69b76e [formerly 05a502f5528d6abab5eb4f0b980e38c2eac44e03]
    Former-commit-id: 445eaefef26e6f5d4ec1ee515589feae2b1a8bd6

--- components/ResposiveImage.js
+++ components/ResposiveImage.js
@@ -6,42 +6,7 @@ import "lazysizes/plugins/attrchange/ls.attrchange";
 const ResposiveImage = ({
   value: { srcSp, srcTb, src, load, name, srcWidth, srcHeight, index },
 }) => {
-  // const [imageWidth, setImageWidth] = useState(null);
-  // const [imageHeight, setImageHeight] = useState(null);
-  // // const [loading, setLoading] = useState(true);
-  // // console.log(imageWidth);
 
-  // const createImageElement = (path) => {
-  //   return new Promise((resolve) => {
-  //     const image = new Image();
-  //     image.onload = () => resolve(image);
-  //     image.src = path;
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   const handleSetOrientation = async () => {
-  //     const image = await createImageElement(src);
-  //     // let arr = [];
-  //     // arr.push(image[index].width);
-  //     // console.log(arr);
-  //     setImageWidth(image.naturalWidth);
-
-  //     const arr = new Map();
-
-  //     arr.set(index, imageWidth);
-
-  //     setImageHeight(image.naturalHeight);
-  //   };
-  //   handleSetOrientation();
-  //   // if (imageWidth != null && imageHeight != null) {
-  //   //   setLoading(false);
-  //   // }
-  // }, [src, imageWidth, imageHeight]);
-
-  // if (loading) {
-  //   return <div>loading</div>;
-  // }
   return (
     <picture>
       <source
@@ -57,12 +22,12 @@ const ResposiveImage = ({
       <source data-srcset={src} type="image/webp" />
       <img
         decoding="async"
-        // src={
-        //   load
-        //     ? srcSp
-        //     : "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-        // }
-        src={srcSp}
+        src={
+          load
+            ? srcSp
+            : "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+        }
+        // src={srcSp}
         className={`fade-in lazyload ${styles.emakiImg}`}
         alt={name}
         width={srcWidth}
