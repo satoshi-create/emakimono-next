7d4fd947d4af49b31c49d59c263fb7e96ba66f62
satoshi <mimizunobouken@gmail.com>
2023-01-14 10:59:53 +0900

    fixed
    Former-commit-id: 6a314a4335513ef73493d979466eae4450ae5b54 [formerly d6b1f2c82f2c5bd11d91d31b9352500acc123cd3]
    Former-commit-id: bfe686de67d0b6ab699001722ac31c44edcd1417

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -26,26 +26,26 @@ export default async function sendMail(req, res) {
     `,
   });
 
-  await transporter.sendMail({
-    from: process.env.MAIL_USER,
-    to: `${req.body.email}`,
-    subject: `【お問い合わせ自動受付メール】`,
-    text: req.body.message + " | Sent from: " + req.body.email,
-    html: `
-          <p>
-            お問い合わせありがとうございます。
-            <br>以下の内容でお問い合わせを承りました。
-          </p>
-          <p>-----------------------------------------</p>
-            <p>【名前】</p>
-            <p>${req.body.name}</p>
-            <p>【メールアドレス】</p>
-            <p>${req.body.email}</p>
-            <p>【メッセージ】</p>
-            <p>${req.body.message.replaceAll("\n", "<br>")}</p>
-          <p>-----------------------------------------</p>
-        `,
-  });
+  // await transporter.sendMail({
+  //   from: process.env.MAIL_USER,
+  //   to: `${req.body.email}`,
+  //   subject: `【お問い合わせ自動受付メール】`,
+  //   text: req.body.message + " | Sent from: " + req.body.email,
+  //   html: `
+  //         <p>
+  //           お問い合わせありがとうございます。
+  //           <br>以下の内容でお問い合わせを承りました。
+  //         </p>
+  //         <p>-----------------------------------------</p>
+  //           <p>【名前】</p>
+  //           <p>${req.body.name}</p>
+  //           <p>【メールアドレス】</p>
+  //           <p>${req.body.email}</p>
+  //           <p>【メッセージ】</p>
+  //           <p>${req.body.message.replaceAll("\n", "<br>")}</p>
+  //         <p>-----------------------------------------</p>
+  //       `,
+  // });
 
   res.status(200).json({
     success: true,
