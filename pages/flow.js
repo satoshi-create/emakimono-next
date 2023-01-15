dd186941dfeebb8e1feedaeeb1954821d1813109
satoshi <mimizunobouken@gmail.com>
2023-01-15 10:46:10 +0900

    ・flowページを作成
    Former-commit-id: a632059e3939f7bdd706d10507cc874b42797899 [formerly 1369888cd1896cd547dcd0992d6485668b76d8c7]
    Former-commit-id: ea3f13ab0ce95d7e35449ca9b49c2facd9469340

---
+++ pages/flow.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "lazysizes";
+import Head from "../components/Meta";
+// import styles from "../styles/Flow.css.module.css";
+import Title from "../components/Title";
+import FullscreenContents from "../components/FullscreenContents";
+import dataEmakis from "../libs/data";
+import { useLocale, useLocaleData } from "../libs/func";
+
+const Flow = ({ cyouzyuuzinbutugiga, seiyoukaiga, suibokuga, mone }) => {
+  const { t } = useLocale();
+  const { t: data } = useLocaleData();
+  return (
+    <>
+      <Head />
+      <Header />
+      <FullscreenContents
+        data={{ cyouzyuuzinbutugiga, seiyoukaiga, suibokuga, mone }}
+        sectiontitle={"流れる巻物"}
+        sectiontitleen={"flow"}
+      />
+      <Footer />
+    </>
+  );
+};
+
+export const getStaticProps = async (context) => {
+  const { locale, locales } = context;
+  // const tEmakisData = locale === "en" ? enData : jaData;
+
+  const cyouzyuuzinbutugiga = dataEmakis.find(
+    (item, index) =>
+      item.title === "鳥獣人物戯画絵巻" && item.edition === "甲巻"
+  );
+
+  const suibokuga = dataEmakis.find(
+    (item, index) => item.title === "四季山水図巻（山水長巻）"
+  );
+  const mone = dataEmakis.find((item, index) => item.title === "睡蓮 連作");
+
+  const seiyoukaiga = dataEmakis.find(
+    (seiyoukaiga) => seiyoukaiga.title === "ブランカッチ礼拝堂 装飾画"
+  );
+
+  return {
+    props: {
+      cyouzyuuzinbutugiga: cyouzyuuzinbutugiga,
+      seiyoukaiga: seiyoukaiga,
+      suibokuga: suibokuga,
+      mone: mone,
+    },
+  };
+};
+
+export default Flow;
