2b816e79076a517e5c76dc5f9fee1e3be5bfc1cc
satoshi <mimizunobouken@gmail.com>
2023-01-17 10:56:36 +0900

    fixed
    Former-commit-id: 37f565ba6cb8e3a7d6cab41dc8f8740f30fbe875 [formerly 3366adf460bd0fc1fb5a0780f9f9ac5d0f89c928]
    Former-commit-id: 66ac75ea8cb4ce98bb707141d19d3c25875e768a

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -11,7 +11,7 @@ export default async function sendMail(req, res) {
     },
   });
 
-  await transporter.sendMail({
+  const toHostMail = {
     from: process.env.USER,
     to: process.env.USER,
     subject: `【お問い合わせ】${req.body.name}様より`,
@@ -24,34 +24,60 @@ export default async function sendMail(req, res) {
       <p>【メッセージ】</p>
       <p>${req.body.message.replaceAll("\n", "<br>")}</p>
     `,
+  };
+
+  const toGuestMail = {
+    from: process.env.MAIL_USER,
+    to: `${req.body.email}`,
+    subject: `【お問い合わせ自動受付メール】`,
+    text: req.body.message + " | Sent from: " + req.body.email,
+    html: `
+          <p>
+            お問い合わせありがとうございます。
+            <br>以下の内容でお問い合わせを承りました。
+          </p>
+          <p>-----------------------------------------</p>
+            <p>【名前】</p>
+            <p>${req.body.name}</p>
+            <p>【メールアドレス】</p>
+            <p>${req.body.email}</p>
+            <p>【メッセージ】</p>
+            <p>${req.body.message.replaceAll("\n", "<br>")}</p>
+          <p>-----------------------------------------</p>
+        `,
+  };
+
+  await transporter.sendMail(toHostMail, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send("error" + JSON.stringify(err));
+    } else {
+      console.log("mail send");
+      res.send("success");
+    }
   });
 
-    await transporter.sendMail({
-      from: process.env.MAIL_USER,
-      to: `${req.body.email}`,
-      subject: `【お問い合わせ自動受付メール】`,
-      text: req.body.message + " | Sent from: " + req.body.email,
-      html: `
-            <p>
-              お問い合わせありがとうございます。
-              <br>以下の内容でお問い合わせを承りました。
-            </p>
-            <p>-----------------------------------------</p>
-              <p>【名前】</p>
-              <p>${req.body.name}</p>
-              <p>【メールアドレス】</p>
-              <p>${req.body.email}</p>
-              <p>【メッセージ】</p>
-              <p>${req.body.message.replaceAll("\n", "<br>")}</p>
-            <p>-----------------------------------------</p>
-          `,
-    });
+  await transporter.sendMail(toGuestMail, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send("error" + JSON.stringify(err));
+    } else {
+      console.log("mail send");
+      res.send("success");
+    }
+  });
 
   res.status(200).json({
     success: true,
   });
 }
 
-
-
-
+// transporter.sendMail(mailOption, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     res.send("error" + JSON.stringify(err));
+//   } else {
+//     console.log("mail send");
+//     res.send("success");
+//   }
+// });
