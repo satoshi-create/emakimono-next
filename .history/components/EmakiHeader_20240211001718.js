4b0dd93ccf6dd5d1aa6941c0c9b8cb6267708e98
satoshi-create <mimizunobouken@gmail.com>
2024-02-11 00:49:28 +0900

    add EmakiHeader component
    Former-commit-id: efb40355a92229a73c22b65de5fe99cceb23d0a9

---
+++ .history/components/EmakiHeader_20240211001718.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "../styles/EmakiHeader.module.css"
+
+const EmakiHeader = () => {
+  return (
+    <header>
+      
+    </header>
+    <div>EmakiHeader</div>
+  )
+}
+
+export default EmakiHeader
