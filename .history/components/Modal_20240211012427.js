7a0f646db2d3adecd3065ceb15b5bc813eb22825
satoshi-create <mimizunobouken@gmail.com>
2024-02-11 01:26:35 +0900

    修正
    Former-commit-id: 2a4b7529bbe491bbccb102fbac1f027aff9af3f9 [formerly 9b4c2669648d59eb75b5a5ea7dbc067aa0b43906]
    Former-commit-id: 36cd189f1c57bf33b1d3d4f3797a963bd4cebb91

--- .history/components/Modal_20240211012427.js
+++ .history/components/Modal_20240211012427.js
@@ -11,8 +11,7 @@ import Link from "next/link";
 import AllLocation from "./AllLocation";
 import Image from "next/image";
 
-// TODO:モーダルに絵巻の情報を表示する
-  
+TODO:
 const Modal = ({ data }) => {
   const { isModalOpen, closeModal, openModal, index, setIndex } =
     useContext(AppContext);
