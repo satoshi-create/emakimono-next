f44e704590e9ced5b1ba24a4d58c88c6f6906253
satoshi-create <mimizunobouken@gmail.com>
2024-02-18 01:01:36 +0900

    Keywors -personnameに画像を追加
    Former-commit-id: e718d0000f08710d088ba780d5a758b7512d7d5a [formerly 187e93bab484171ec57195abdead318db39e072d]
    Former-commit-id: d4d807cf55574fefa586d58ca75c885a34a5c037

---
+++ .history/components/Keywords_20240218003117.js
@@ -0,0 +1,74 @@
+import React from "react";
+import Title from "./Title";
+import emakisData from "../libs/data";
+import Link from "next/link";
+import styles from "../styles/Tags.module.css";
+import { keywordItem, personnameItem } from "../libs/func";
+import { useRouter } from "next/router";
+import Button from "./Button";
+
+console.log(emakisData);
+
+const Keywords = ({ sectiontitle, path }) => {
+  const { locale } = useRouter();
+  if (sectiontitle === "キーワード") {
+    return (
+      <section className={`section-center section-padding ${styles.container}`}>
+        <section
+          className={`${styles.tags} ${locale === "ja" && styles.jatags}`}
+        >
+          {keywordItem(emakisData).map((item, index) => {
+            const { name, id, slug, total, ruby } = item;
+
+            return (
+              <div
+                key={index}
+                className={`${styles.taginfo} ${
+                  locale === "en" && styles.keywordsen
+                }`}
+              >
+                <Link href={`./${path}/${slug}`}>
+                  <a className={styles.title}>{locale === "en" ? id : name}</a>
+                </Link>
+                <div className={styles.total}>{`(${total})`}</div>
+              </div>
+            );
+          })}
+        </section>
+      </section>
+    );
+  } else {
+    return (
+      <section className={`section-center section-padding ${styles.container}`}>
+        <section
+          className={`${styles.tags} ${locale === "ja" && styles.jatags}`}
+        >
+          {personnameItem(emakisData).map((item, index) => {
+            const { name, id, slug, total, ruby, portrait } = item;
+            return (
+              // <div
+              //   key={index}
+              //   className={`${styles.taginfo} ${
+              //     locale === "en" && styles.personnames
+              //   }`}
+              // >
+              <>
+                <Link href={`./${path}/${slug}`}>
+                  <a className={styles.title}>
+                    {portrait && <img src={portrait} alt={name} />}
+                    
+                    {locale === "en" ? id : name}
+                    <span className={styles.total}>{`(${total})`}</span>
+                  </a>
+                </Link>
+              </>
+              // </div>
+            );
+          })}
+        </section>
+      </section>
+    );
+  }
+};
+
+export default Keywords;
---
+++ .history/components/ToggleTag_20240218000658.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import Keywords from "./Keywords";
+import Title from "./Title";
+import styles from "../styles/ToggleTag.module.css";
+import emakisData from "../libs/data";
+import { keywordItem, personnameItem } from "../libs/func";
+import { useRouter } from "next/router";
+import Button from "./Button";
+
+const ToggleTag = ({ sectiontitle, sectiontitleen }) => {
+  const { locale } = useRouter();
+  const [value, setValue] = useState(0);
+  const allPersonNames = personnameItem(emakisData);
+  const allKeywords = keywordItem(emakisData);
+
+  const allTag = [
+    {
+      tagtitle: "キーワード",
+      tagtitleen: "keywords",
+      allTags: allKeywords,
+      path: "keyword",
+    },
+    {
+      tagtitle: "人物名",
+      tagtitleen: "personnames",
+      allTags: allPersonNames,
+      path: "personname",
+    },
+  ];
+
+  const { tagtitle, tagtitleen, allTags, path } = allTag[value];
+
+  return (
+    <section className="section-center section-padding">
+      <Title sectiontitle={sectiontitle} sectiontitleen={sectiontitleen} />
+      <div className={styles.tabcontainer}>
+        {allTag.map((item, index) => {
+          const { tagtitle, tagtitleen } = item;
+          return ( 
+            <button
+              onClick={() => setValue(index)}
+              className={`btn ${styles.tabbtn} ${
+                value === index ? styles.activebtn : ""
+              }`}
+              key={index}
+            >
+              {locale === "en" ? tagtitleen : tagtitle}
+            </button>
+          );
+        })}
+      </div>
+      <Keywords
+        sectiontitle={tagtitle}
+        sectiontitleen={tagtitleen}
+        allTags={allTags}
+        path={path}
+      />
+      {tagtitle === "キーワード" ? (
+        <Button
+          title={
+            locale === "en"
+              ? "View a list of keywords !!"
+              : "キーワード一覧を見る"
+          }
+          path={"/keywords"}
+          style={"tag"}
+        />
+      ) : (
+        <Button
+          title={
+            locale === "en"
+              ? "View a list of personnames !!"
+              : "人物名一覧を見る"
+          }
+          path={"/personnames"}
+          style={"tag"}
+        />
+      )}
+    </section>
+  );
+};
+
+export default ToggleTag;
