b5eb9ce2ab89f2dabaffea259075ae607ff0fb0c
satoshi-create <mimizunobouken@gmail.com>
2024-02-18 11:24:10 +0900

    ・「索引から見る絵巻」を「人物から見る絵巻」「索引から見る絵巻」2つのセクションに切り分け

--- components/Title.js
+++ components/Title.js
@@ -3,10 +3,10 @@ import styles from "../styles/Title.module.css";
 
 const Title = ({ sectiontitle, sectiontitleen }) => {
   return (
-    <section className={styles.title}>
+    <div className={styles.title}>
       <h3>{sectiontitleen}</h3>
       <h2>{sectiontitle}</h2>
-    </section>
+    </div>
   );
 };
 
