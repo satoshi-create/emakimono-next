53053727d3cbf433904177858b25a42bc182c767
satoshi-create <mimizunobouken@gmail.com>
2024-02-21 12:24:35 +0900

    fixed
    Former-commit-id: 99f29c7307b65c5efb8700d6060789f903a00fea [formerly 5f8a5d3ca3e975a9857a8de2c146b77c75658d1e]
    Former-commit-id: e56ed0ba7a27d76236c48931448d7836f913f8c5

--- components/EmakiInfo.js
+++ components/EmakiInfo.js
@@ -8,8 +8,6 @@ import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 
 // TODO:絵詞を参照から出典に変更
-
-
 // TODO:現在位置（段、紙、長さ）を表示する機能を追加する
 const EmakiInfo = ({ value }) => {
   const { openModal } = useContext(AppContext);
