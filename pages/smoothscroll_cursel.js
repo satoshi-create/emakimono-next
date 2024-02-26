179a9b706c60de75ddae1d7d42db39f242c0f5c7
satoshi-create <mimizunobouken@gmail.com>
2024-02-27 00:45:28 +0900

    EmakiNavigationコンポーネントを作成
    Former-commit-id: 659ec4f8cd7cb3e54cc897ee679e53b36ad6a36f

--- pages/smoothscroll_cursel.js
+++ pages/smoothscroll_cursel.js
@@ -1,19 +1,35 @@
+import { border } from "@chakra-ui/react";
 import { useRef, useState } from "react";
 import { flushSync } from "react-dom";
 
 export default function CatFriends() {
   const selectedRef = useRef(null);
   const [index, setIndex] = useState(0);
-
-  function handleCursel() {
+  const endIndex = catList.length - 1;
+  console.log(index);
+  function handleCurselNext() {
     flushSync(() => {
       if (index < catList.length - 1) {
         setIndex(index + 1);
       } else {
-        setIndex(0);
+        setIndex(endIndex);
       }
     });
-    console.log(index);
+
+    console.log(selectedRef.current);
+    selectedRef.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+  function handleCurselPrev() {
+    flushSync(() => {
+      if (index <= 0) {
+        setIndex(0);
+      } else {
+        setIndex(index - 1);
+      }
+    });
+
     console.log(selectedRef.current);
     selectedRef.current.scrollIntoView({
       behavior: "smooth",
@@ -21,7 +37,9 @@ export default function CatFriends() {
   }
 
   function handleToId(id) {
-    setIndex(id);
+    flushSync(() => {
+      setIndex(id);
+    });
     selectedRef.current.scrollIntoView({
       behavior: "smooth",
     });
@@ -29,17 +47,31 @@ export default function CatFriends() {
 
   return (
     <div>
-      <Navigation handleCursel={handleCursel} handleToId={handleToId} />
+      <Navigation
+        handleCurselNext={handleCurselNext}
+        handleToId={handleToId}
+        handleCurselPrev={handleCurselPrev}
+        endIndex={endIndex}
+      />
       <List selectedRef={selectedRef} index={index} />
     </div>
   );
 }
 
-const Navigation = ({ handleCursel, handleToId }) => {
+const Navigation = ({
+  handleCurselNext,
+  handleToId,
+  handleCurselPrev,
+  endIndex,
+}) => {
   return (
     <nav style={{ position: "absolute", right: "0" }}>
-      <button onClick={() => handleCursel()}>Next</button>
-      <button onClick={() => handleToId(9)}>Next</button>
+      <button onClick={() => handleToId(endIndex)}>go to end</button>
+      <button onClick={() => handleCurselNext()}>Next</button>
+      <button onClick={() => handleToId(9)}>Jellylorum</button>
+      <button onClick={() => handleToId(5)}>Maru</button>
+      <button onClick={() => handleCurselPrev()}>Prev</button>
+      <button onClick={() => handleToId(0)}>go to first</button>
     </nav>
   );
 };
