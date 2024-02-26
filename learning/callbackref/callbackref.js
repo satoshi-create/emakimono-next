ff8712f4bb9e653c1ec04c66247319134d0b7cde
satoshi-create <mimizunobouken@gmail.com>
2024-02-27 03:57:48 +0900

    learningフォルダを整理
    Former-commit-id: bdf4b7244ed17949b7781f8dd32e5ab8fdece93b [formerly 8bc6dbc0846f4a8a46ab84ec2c3ce3fe080acbdb]
    Former-commit-id: df8e39ff5d1764afcdbb4e8e30e991cd56b248b0

---
+++ learning/callbackref/callbackref.js
@@ -0,0 +1,20 @@
+import { useCallback,useState } from "react";
+
+function MeasureExample() {
+  const [height, setHeight] = useState(0);
+
+  const measuredRef = useCallback((node) => {
+    if (node !== null) {
+      setHeight(node.getBoundingClientRect().height);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1 ref={measuredRef}>Hello, world</h1>
+      <h2>The above header is {Math.round(height)}px tall</h2>
+    </>
+  );
+}
+
+export default MeasureExample
