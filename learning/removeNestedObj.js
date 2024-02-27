d234b66f4714acf990d79844d1dd37e686358d78
satoshi-create <mimizunobouken@gmail.com>
2024-02-27 18:15:05 +0900

    category/[slug].js : which exceeds the threshold of 128 kB, this amount of data can reduce performance. ⇒getStaticPropsに、ネストされたオブジェクトを削除する関数（removeNestedObj）を追加し、データ量を削減
    Former-commit-id: d55ff7bd76be1a9c4b36b26e9bd491509afd1145 [formerly 61136c3b2e6dbbb9c7fb4e6c81979355e4ce7fc1]
    Former-commit-id: 578309208cbb2a43105823225d4b3946039ee96f

---
+++ learning/removeNestedObj.js
@@ -0,0 +1,15 @@
+// https://hi97.hamazo.tv/e8537787.html
+const removeNestedObj = Object.entries(obj).reduce(
+  (acc, [key, val]) => {
+    if ("object" === typeof val) {
+      console.log(acc);
+
+      return acc;
+    }
+    acc[key] = val;
+    return acc;
+  },
+  初期値,
+  空のオブジェクト,
+  {}
+);
