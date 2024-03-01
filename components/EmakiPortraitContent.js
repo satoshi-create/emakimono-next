43040b4c2e0627d8d9ed86d02c6b3ec6b07eb762
satoshi-create <mimizunobouken@gmail.com>
2024-03-01 18:18:10 +0900

    EmakiPortraitContentコンポーネントにスタイルを追加
    Former-commit-id: fde39eadf7d9af1a0074cc66a4be80b321a3249b [formerly 76937ffc4f5f5d9e03aa3941ce36ba6471151963]
    Former-commit-id: 961db8efab149030aaa6765d2218c099005c13b3

--- components/EmakiPortraitContent.js
+++ components/EmakiPortraitContent.js
@@ -1,31 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "../styles/EmakiPortraitContent.module.css";
+import { AppContext } from "../pages/_app";
 
 const EmakiPortraitContent = ({ data }) => {
+  const { handleToId } = useContext(AppContext);
   const { locale } = useRouter();
   const { type, typeen, eraen, era, title, edition, author, desc, emakis } =
     data;
+
+  const filterDesc = desc.substring(0, 40);
+  const descTemp = `${title} ${
+    author && `（${author}）`
+  }の全シーンを、縦書き、横スクロールで楽しむことができます。`;
   return (
-    <div className={styles.metadata}>
-      <div className={styles.cat}>
-        <Link href={`/category/${typeen}`}>
-          <a className={styles.type}>{locale === "en" ? typeen : type}</a>
-        </Link>
-        <Link href={`/era/${eraen}`}>
-          <a className={`era ${styles.era}`}>
-            {locale === "en" ? `${eraen} period` : `${era}時代`}
-          </a>
-        </Link>
+    <div className={styles.container}>
+      <div className={styles.metadataA}>
+        <div className={styles.cat}>
+          <Link href={`/category/${typeen}`}>
+            <a className={styles.type}>{locale === "en" ? typeen : type}</a>
+          </Link>
+          <Link href={`/era/${eraen}`}>
+            <a className={`era ${styles.era}`}>
+              {locale === "en" ? `${eraen} period` : `${era}時代`}
+            </a>
+          </Link>
+        </div>
+        <h3 className={styles.title}>
+          {title}　{edition}
+        </h3>
+        <h4 className={styles.author}>
+          {author
+            ? author
+            : `${locale == "en" ? "artist unknown" : "絵師不詳"}`}
+        </h4>
       </div>
-      <h3 className={styles.title}>
-        {title}　{edition}
-      </h3>
-      <h4 className={styles.author}>
-        {author ? author : `${locale == "en" ? "artist unknown" : "絵師不詳"}`}
-      </h4>
-      <div className={styles.desc}>{desc}</div>
+
+      <div className={styles.desc}>
+        {desc ? `${filterDesc}...もっと見る` : descTemp}
+      </div>
+
       <ul className={styles.mokuji}>
         {emakis.map((item, index) => {
           const { cat, chapter } = item;
