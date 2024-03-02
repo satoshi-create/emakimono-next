bc55b4d0490d3dde4ef756b69d5200cb86ef08d0
satoshi-create <mimizunobouken@gmail.com>
2024-03-03 03:30:23 +0900

    横スクロール時に縦スクロールを防ぐ機能を実装（Prevent window vertical scroll, until div's horizontal scroll reaches its end）

--- components/EmakiConteiner.js
+++ components/EmakiConteiner.js
@@ -12,6 +12,7 @@ import EmakiInfo from "../components/EmakiInfo";
 import EmakiNavigation from "../components/EmakiNavigation";
 import Modal from "./Modal";
 import { NodeNextRequest } from "next/dist/server/base-http/node";
+import $ from "jquery";
 
 const EmakiConteiner = ({
   data,
@@ -37,29 +38,99 @@ const EmakiConteiner = ({
 
   const [toggle, setToggle] = useState(true);
 
+  const wrapperRef = useRef();
   const articleRef = useRef();
   const scrollNextRef = useRef(null);
   const scrollPrevRef = useRef(null);
 
+  // useEffect(() => {
+  //   if (scroll) {
+  //     const el = articleRef.current;
+  //     if (el) {
+  //       const wheelListener = (e) => {
+  //         console.log(e.deltaY);
+
+  //         e.preventDefault();
+  //         el.scrollTo({
+  //           left: el.scrollLeft + e.deltaY * 3,
+  //           behavior: "smooth",
+  //         });
+  //       };
+
+  //       el.addEventListener("wheel", wheelListener, { passive: true });
+  //       el.addEventListener("touchmove", wheelListener, { passive: false });
+  //     }
+  //   }
+  // }, [articleRef, scroll]);
+
+  // https://stackoverflow.com/questions/55152799/prevent-window-vertical-scroll-until-divs-horizontal-scroll-reaches-its-end
+  // useEffect(() => {
+  //   const wr = wrapperRef.current;
+  //   const el = articleRef.current;
+  //   var scroller = {};
+  //   scroller.e = document.getElementById("scroll");
+
+  //   function MouseWheelHandler(e) {
+  //     // // cross-browser wheel delta
+  //     // var e = window.event || e;
+  //     var delta = -30 * Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+
+  //     var pst = $("#scroll").scrollLeft() + delta;
+
+  //     if (pst < 0) {
+  //       pst = 0;
+  //     } else if (pst > $(".box-wrap").width()) {
+  //       pst = $(".box-wrap").width();
+  //     }
+  //     console.log(pst);
+
+  //     $("#scroll").scrollLeft(pst);
+  //     e.preventDefault(); // << add this
+  //     e.stopPropagation(); // << add this
+  //     return false;
+  //   }
+
+  //   scroller.e.addEventListener("mousewheel", MouseWheelHandler, false);
+  // }, []);
+
+  // https://stackoverflow.com/questions/55152799/prevent-window-vertical-scroll-until-divs-horizontal-scroll-reaches-its-end
   useEffect(() => {
-    if (scroll) {
-      const el = articleRef.current;
-      if (el) {
-        const wheelListener = (e) => {
-          console.log(e.deltaY);
+    let scrollSpeed = 50;
+    const el = articleRef.current;
 
-          e.preventDefault();
-          el.scrollTo({
-            left: el.scrollLeft + e.deltaY * 3,
-            behavior: "smooth",
-          });
-        };
+    const MouseWheelHandler = (e) => {
+      // block if e.deltaY==0
+      // 垂直方向のスクロールがゼロならばリターン
+      if (!e.deltaY) return;
+      // Set scrollDirection (-1 = up // 1 = down)
+      let scrollDirection = e.deltaY > 0 ? 1 : -1;
+      // convert vertical scroll into horizontal
+      // 縦スクロールを横スクロールに変換
+      el.scrollLeft += scrollSpeed * scrollDirection;
+      let scrollLeft = Math.round(el.scrollLeft);
+      // calculate box total vertical scroll
+      // ボックス全体の垂直スクロール（水平スクロール）を計算する;
+      let maxScrollLeft = Math.round(el.scrollWidth - el.clientWidth);
+      // console.log(el.clientWidth);
+      // // 818;
+      // console.log(el.scrollWidth);
+      // // 25202;
+      // console.log(maxScrollLeft);
+      // // 24384;
+      // if element scroll has not finished scrolling
+      // prevent window to scroll
+      // ウィンドウがスクロールしないようにする;
+      if (
+        (scrollDirection === -1 && scrollLeft > 0) ||
+        (scrollDirection === 1 && scrollLeft < maxScrollLeft)
+      )
+        e.preventDefault();
+      // done!
+      return true;
+    };
 
-        el.addEventListener("wheel", wheelListener, { passive: true });
-        el.addEventListener("touchmove", wheelListener, { passive: false });
-      }
-    }
-  }, [articleRef, scroll]);
+    el.addEventListener("mousewheel", MouseWheelHandler, false);
+  }, []);
 
   // イベントリスナーを使う方法で実装。
   // イベントハンドラーを使う方法は（scrollevent_eventhandler）内
