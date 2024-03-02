66db7f11c5edd76749af329b19f2d43a24beea4d
satoshi-create <mimizunobouken@gmail.com>
2024-03-02 17:36:37 +0900

    EmakiLandscapContentコンポーネントにチャプターを追加
    Former-commit-id: 37ad39230e9df11aab210da991de3862ef5e037d

--- components/EmakiLandscapContent.js
+++ components/EmakiLandscapContent.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import EmakiConteiner from "./EmakiConteiner";
 import styles from "../styles/EmakiLandscapContent.module.css";
+import { AppContext } from "../pages/_app";
 
 const EmakiLandscapContent = ({
   data,
@@ -9,6 +10,8 @@ const EmakiLandscapContent = ({
   articleRef,
   height,
 }) => {
+  const { handleToId } = useContext(AppContext);
+  const { emakis } = data;
   return (
     <div className={`emaki-page-landscape-grid`}>
       <div className={styles.wrapper}>
@@ -20,7 +23,24 @@ const EmakiLandscapContent = ({
           articleRef={articleRef}
           height={height}
         />
-        <p>aside components</p>
+        <ul className={styles.chapter}>
+          <h4 className={styles.chapterTitle}>目次</h4>
+          <span className={styles.chapterBorderline}></span>
+          {emakis.map((item, index) => {
+            const { cat, chapter } = item;
+            if (cat === "ekotoba") {
+              return (
+                <li key={index}>
+                  <span
+                    onClick={() => handleToId(index)}
+                    className={styles.chapterlink}
+                    dangerouslySetInnerHTML={{ __html: chapter }}
+                  ></span>
+                </li>
+              );
+            }
+          })}
+        </ul>
       </div>
     </div>
   );
