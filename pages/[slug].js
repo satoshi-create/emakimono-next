e36f7c96ab377152b616e7c9a13d4f46810f89e9
satoshi-create <mimizunobouken@gmail.com>
2024-03-04 02:54:58 +0900

    アンカー先へスムーズスクロールした状態でページ遷移すると、同じ位置までスムーズスクロールしてしまう不具合の修正
    Former-commit-id: 7fc64bed9b881f15439cdda1e25e37ea7e485374 [formerly d6dddb80b46855238ec71d235776387c0b6a4bdf]
    Former-commit-id: 6e65de099d023a229be1fb51816661b8980f5458

--- pages/[slug].js
+++ pages/[slug].js
@@ -87,10 +87,11 @@ const Emaki = ({ data, locale, locales, slug }) => {
   console.log(navIndex);
 
   useEffect(() => {
-    return () => {
-      setnavIndex(0);
-      setHash(0);
-    };
+    //qiita.com/7280ayubihs/items/0d359c3a3b5bc8a4b6fd
+    // 画面遷移した際に、スクロール位置をリセット
+    https: window.scrollTo({ top: 0, behavior: "instant" });
+    setnavIndex(0);
+    setHash(0);
   }, [setnavIndex, setHash]);
 
   // const matchMediaContainer = (ori) => {
