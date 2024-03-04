7734a6590a1fc217ae52ff5ca914fc0adaef85a6
satoshi-create <mimizunobouken@gmail.com>
2024-03-04 21:36:10 +0900

    fix
    Former-commit-id: 92db700834479b30c4ab2b3c41993c6e171448a2 [formerly e7902cbe164f4ea2f4076f056c9fe5ffa0ddc3df]
    Former-commit-id: 16e711ee1bab46063ec57694038b8cb9e4ee66fa

--- components/EmakiNavigation.js
+++ components/EmakiNavigation.js
@@ -14,7 +14,8 @@ import { useRouter } from "next/router";
 import ToggleEkotoba from "./ToggleEkotoba";
 import { AppContext } from "../pages/_app";
 
-// TODO: 「先頭に戻る」を押しても反応がない
+// TODO: 横スクロールで最後まで進み、「先頭に戻る」を押しても反応がない
+// ⇒navIndexが0になっている
 const EmakiNavigation = ({
   handleToId,
   data,
