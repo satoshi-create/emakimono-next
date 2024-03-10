fc69664b0c81cc443138a8f23aafe73bbaf702e5
satoshi-create <mimizunobouken@gmail.com>
2024-03-10 14:55:11 +0900

    FullScreenComp - 源氏物語54帖のタイトルにrubyを作成 - 源氏物語54帖のタイトルの配列に、flatMap,joinを使ってスペースを挿入
    Former-commit-id: 1ceefcc17fb7ce96f5194970cd6b3d4a0a09035d

--- components/FullScreenComp.js
+++ components/FullScreenComp.js
@@ -9,6 +9,13 @@ import Link from "next/link";
 
 // TODO:CREATE - 「流れる絵巻」をフルスクリーンにすると横向きになる機能を実装する or ライブラリを使わない方式に切り替える
 
+// https://stackoverflow.com/questions/34034038/how-to-render-react-components-by-using-map-and-join
+// const Demo = ({ data }) => (
+//   <div>
+//     {data.flatMap((t, i) => [...(i ? [", "] : []), <span key={i}>{t}</span>])}
+//   </div>
+// );
+
 export default function FullScreenComp({ children, index, genjieslug }) {
   const handle = useFullScreenHandle();
   console.log(genjieslug);
@@ -20,16 +27,38 @@ export default function FullScreenComp({ children, index, genjieslug }) {
         }`}
       >
         <div className={`${styles.genjieslugBox}`}>
+          {genjieslug.flatMap((item, i) => [
+            ...(i ? ["　"] : []),
+            <h4 className={`${styles.genjieslugTitle}`} key={i}>
+              <Link href={`/genjie/${item.path}`}>
+                <a>
+                  <ruby>
+                    {item.title} <rp>(</rp>
+                    <rt className={styles.rt}>{item.ruby}</rt>
+                    <rp>)</rp>
+                  </ruby>
+                </a>
+              </Link>
+            </h4>,
+          ])}
+        </div>
+        {/* <div className={`${styles.genjieslugBox}`}>
           {genjieslug.map((item, i) => {
             return (
               <h4 className={`${styles.genjieslugTitle}`} key={i}>
                 <Link href={`/genjie/${item.path}`}>
-                  <a>{item.title}</a>
+                  <a>
+                    <ruby>
+                      {item.title} <rp>(</rp>
+                      <rt className={styles.rt}>{item.ruby}</rt>
+                      <rp>)</rp>
+                    </ruby>
+                  </a>
                 </Link>
               </h4>
             );
           })}
-        </div>
+        </div> */}
         <button
           onClick={handle.enter}
           className={styles.openIcon}
