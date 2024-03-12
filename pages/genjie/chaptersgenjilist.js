471e848b3d6dcce7d20ceb534b784804836d5cf1
satoshi-create <mimizunobouken@gmail.com>
2024-03-12 18:03:36 +0900

    Modalコンポーネントにスムーズスクロールを追加
    Former-commit-id: 5badec2f2fa525c609e45225ce44b3593057d3eb [formerly 584e61e0f8ca2fd45d59a673650226743b5ab2e4]
    Former-commit-id: fb7a5c2aadcf8585e9a0baad073e5f41b8108ff7

--- pages/genjie/chaptersgenjilist.js
+++ pages/genjie/chaptersgenjilist.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ChaptersTable from "../../components/ChaptersTable";
 import { genjieSlugItem, useLocaleData } from "../../libs/func";
-import ExtractingListData from "../../components/ExtractingListData";
+import ExtractingListData from "../../libs/ExtractingListData";
 import Head from "../../components/Meta";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
