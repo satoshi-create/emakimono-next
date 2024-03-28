a5c0691fcc8f00a912e4be1d20a60c33894803c6
satoshi-create <mimizunobouken@gmail.com>
2024-03-28 09:28:00 +0900

    絵師名（author）でダイナミックルーティングを作成
    Former-commit-id: 1098a04346f3db6d2a3e0ec5362be7be91e7745a

---
+++ pages/author/[slug].js
@@ -0,0 +1,78 @@
+import React from "react";
+import emakisData from "../../libs/data";
+import { authorItem, removeNestedEmakisObj } from "../../libs/func";
+import enData from "../../libs/en/data";
+import jaData from "../../libs/data";
+import Head from "../../components/Meta";
+import Header from "../../components/Header";
+import Breadcrumbs from "../../components/Breadcrumbs";
+import CardA from "../../components/CardA";
+import Footer from "../../components/Footer";
+import { useRouter } from "next/router";
+
+const Author = ({ name, nameen, posts, slug }) => {
+  const { locale } = useRouter();
+  const tPageDesc =
+    locale === "en"
+      ? `This is the ${nameen} list page.This site pursues the enjoyment of picture scrolls by scrolling from right to left!`
+      : `${name}のページです。縦書き、横スクロールで、絵巻物本来の見方を楽しむことを追求しているサイトです。`;
+
+  return (
+    <>
+      <Head pagetitle={locale === "en" ? nameen : name} pageDesc={tPageDesc} />
+      <Header />
+      <Breadcrumbs name={locale === "en" ? nameen : name} />
+      <CardA
+        emakis={posts}
+        columns={"three"}
+        sectionname={"recommend"}
+        sectiontitle={locale === "en" ? nameen : name}
+        sectiontitleen={locale === "en" ? name : nameen}
+      />
+      <Footer />
+    </>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const paths = authorItem(emakisData).map(({ authoren }) => ({
+    params: {
+      slug: authoren,
+    },
+    locale: "ja",
+  }));
+  paths.push(...paths.map((item) => ({ ...item, locale: "en" })));
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const authorslug = context.params.slug;
+  const { locale, locales } = context;
+  const tEmakisData = locale === "en" ? enData : jaData;
+  console.log(authorItem(tEmakisData));
+  const authorObj = authorItem(tEmakisData).find(
+    ({ authoren }) => authoren === authorslug
+  );
+
+  const removeNestedArrayObj = tEmakisData.map((item) => {
+    return removeNestedEmakisObj(item);
+  });
+
+  const filterdEmakisData = removeNestedArrayObj.filter(
+    ({ authoren }) => authoren === authorslug
+  );
+
+  return {
+    props: {
+      name: authorObj.author,
+      nameen: authorObj.authoren,
+      posts: filterdEmakisData,
+      slug: authorslug,
+    },
+  };
+};
+
+export default Author;
