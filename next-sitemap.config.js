40adaa4a1286d7d7e4dc239cc3601eb6f576b63c
satoshi-create <mimizunobouken@gmail.com>
2024-04-02 13:33:51 +0900

    publicフォルダにsitemapを生成

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,11 @@
 const config = {
   siteUrl: `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}`,
+  // TODD : スクレイピング対策のためsitemapの名前を変更する
+  // https://ph-1ab.com/exclude-specific-url-in-next-sitemap/
+  // https://zenn.dev/masa5714/articles/b00f4ebffbbcd4
+  // sitemapBaseFileName:"sitemap-9?4kanbb1&&",
   generateRobotsTxt: true,
-  outDir: "./out",
+  // outdir: "./out",
   // sitemapSize: 7000,
   // transform: async (config, path) => {
   //   return {
@@ -14,6 +18,5 @@ const config = {
 };
 
 module.exports = config;
-
 
 // https://yukimasablog.com/nextjs-sitemap
