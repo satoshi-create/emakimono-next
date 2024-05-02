4b0efbad3e520f81c85f08db7391d5b38312df88
satoshi-create <mimizunobouken@gmail.com>
2024-05-02 19:27:07 +0900

    ・EmakiLandscapContentコンポーネントにTwitter、Facebookのシェアボタンを作成
    Former-commit-id: ae5a04c898946a2cbdee2c467726bf4e10f9c8a9 [formerly f0bca99bb1937e4588e56fc076ffa909a3deedd5]
    Former-commit-id: 8c679e5ac46a4b88fcbeea39376514c79af9f8a4

---
+++ libs/sns-share-data.js
@@ -0,0 +1,9 @@
+export const snsShareData = [
+  {
+    // path: `https://twitter.com/share?url=${
+    //   encodeUrl ? encodeUrl : "https://emakimono.com/"
+    //   }&text=${title} ${edition}`,
+    name:"twitter",
+    icon: "faTwitter",
+  },
+];
