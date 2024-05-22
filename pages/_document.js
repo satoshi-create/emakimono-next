4b88a27f1d43bd92cf7a6933110adb0168dd2304
satoshi-create <mimizunobouken@gmail.com>
2024-05-23 01:27:40 +0900

    ダイナミックルーティングのメタ情報を修正

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           /> */}
           {/* Noto Sans Japaneseを追加 */}
           <link
-            href="https://fonts.googleapis.com/css2?family=Kaisei+Decol:wght@400;500;700&family=Kaisei+HarunoUmi:wght@400;500;700&family=Noto+Sans+JP:wght@300;400;500&family=Zen+Kurenaido&family=Zen+Maru+Gothic:wght@300;400;500;700&display=swap&family=Noto+Serif+JP&display=swap&family=Noto+Serif+JP&family=Zen+Kaku+Gothic+New&family=Shippori+Mincho&family=Yomogi&display=swap&family=Shippori+Mincho&display=swap&family=Hina+Mincho&family=Shippori+Mincho&display=swap')"
+            href="https://fonts.googleapis.com/css2?family=Kaisei+Decol:wght@400;500;700&family=Kaisei+HarunoUmi:wght@400;500;700&family=Noto+Sans+JP:wght@300;400;500&family=Zen+Kurenaido&family=Zen+Maru+Gothic:wght@300;400;500;700&display=swap&family=Noto+Serif+JP&display=swap&family=Noto+Serif+JP&family=Zen+Kaku+Gothic+New&family=Shippori+Mincho&family=Yomogi&display=swap&family=Shippori+Mincho&display=swap&family=Hina+Mincho&family=Shippori+Mincho&display=swap&family=Caveat:wght@400..700&family=Dancing+Script:wght@400..700&family=Noto+Serif+JP:wght@200..900&family=RocknRoll+One&display=swap')"
             rel="stylesheet"
           />
         </Head>
