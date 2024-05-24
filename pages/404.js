cd59015e92b1a4d8509e5e6c01e7c7fe822cfa06
satoshi-create <mimizunobouken@gmail.com>
2024-05-24 12:41:20 +0900

    gtm

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Head from "../components/Meta";
+
+const Cusom404 = () => {
+  return (
+    <main>
+      <Head pagetitle={"404 - page not found"} />
+      <Header fixed={false} />
+      <h1>404 page not found</h1>
+      <h4>ページが見つかりません</h4>
+      {/* <Footer /> */}
+    </main>
+  );
+};
+
+export default Cusom404;
