a26913e521e6f03d662f67eee0a2dc37db545eb1
satoshi-create <mimizunobouken@gmail.com>
2024-06-08 11:41:37 +0900

    fix

--- components/EditionLinks.js
+++ components/EditionLinks.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { useLocaleData } from "../libs/func";
+import { useLocaleData} from "../libs/func";
+import { useRouter } from "next/router";
 import { ExternalLink } from "react-feather";
 import Link from "next/link";
 import styles from "../styles/EditionLinks.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faScroll
+} from "@fortawesome/free-solid-svg-icons";
 
 const EditionLinks = ({ title, edition }) => {
+  const { locale } = useRouter();
   const { t: alldata } = useLocaleData();
   const editionLinks = alldata.filter(
     (item) => item.title === title && item.edition !== edition
@@ -19,12 +25,29 @@ const EditionLinks = ({ title, edition }) => {
               <li key={i}>
                 <Link href={`./${item.titleen}`}>
                   <a target="_blank">
-                    <p>
-                      {item.title} {item.edition}
-                      {item.edition !== edition && (
-                        <ExternalLink className={styles.ExternalLinkIcon} />
-                      )}
-                    </p>
+                    {locale == "en" ? (
+                      <p>
+                        <FontAwesomeIcon
+                          icon={faScroll}
+                          className={styles.scrollIcon}
+                        />
+                        {item.titleen}
+                        {item.edition !== edition && (
+                          <ExternalLink className={styles.ExternalLinkIcon} />
+                        )}
+                      </p>
+                    ) : (
+                      <p>
+                        <FontAwesomeIcon
+                          icon={faScroll}
+                          className={styles.scrollIcon}
+                        />
+                        {item.title} {item.edition}
+                        {item.edition !== edition && (
+                          <ExternalLink className={styles.ExternalLinkIcon} />
+                        )}
+                      </p>
+                    )}
                   </a>
                 </Link>
               </li>
