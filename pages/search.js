8d677c5865102150f4ba1904796717e4542539de
satoshi-create <mimizunobouken@gmail.com>
2024-06-12 00:43:06 +0900

    ・タイプ選択のドロップダウンメニューを実装
    Former-commit-id: ba1c9c818196f7a736e1e24d7a8f4be8759d99c4 [formerly b5073f45ad5257865e3eaf9eeaac02d9e9feb808]
    Former-commit-id: 5a5068a12d683735561e49072c16f6ed0a35cd91

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,4 @@
-import React,{useContext} from "react";
+import React, { useContext,useState } from "react";
 import Head from "../components/Meta";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -9,22 +9,32 @@ import SearchForm from "../components/SearchForm";
 import { AppContext } from "../pages/_app";
 
 const Search = () => {
-      const { searchKeyword } = useContext(AppContext);
+  const { searchKeyword } = useContext(AppContext);
   const { locale } = useRouter();
   const removeNestedArrayObj = ExtractingListData();
   const data = removeNestedArrayObj;
+
+
   // console.log(data);
 
-
- // const searchKeyword = "人"
+  // const searchKeyword = "人"
   // const filteredData = data.filter(item => item.titleen === searchkeywod)
   // console.log(filteredData);
 
-  const filteredData = data.filter((item) => {
-    return new RegExp(searchKeyword).test(item.title);
-  });
+  const categories = Array.from(new Set(data.map((item) => item.type)));
+  console.log(categories);
+  
+  const selectCategory = (category) => {
+    return filter((item) => item.category === category);
+  }
 
-  console.log(filteredData);
+  const regx = new RegExp(searchKeyword);
+
+  const filteredData = showData.filter((item) => {
+    const title = item.title + item.edition + item.titleen;
+    const data = regx.test(title);
+    setShowdData(data)
+  });
 
   //himenon.github.io/docs/javascript/simple-react-local-search-form/
   // ["a", "b", "c"].filter((text) => {
@@ -33,19 +43,27 @@ const Search = () => {
 
   // RegExp.prototype.test();
 
-return (
+  return (
     <>
       <Head />
       <Header />
-      <SearchForm/>
+      <SearchForm />
+      {categories.map((category, i) => (
+        <div key={i}>
+          <button onClick={() => selectCategory(category)}>{category}</button>
+        </div>
+      ))}
+      <p>検索結果：{showData.length}件</p>
+      {showData.length > 0 && (
+        <CardA
+          emakis={showData}
+          columns={"four"}
+          // sectionname={"recommend"}
+          // sectiontitle={locale === "en" ? `List of ${nameen}` : `${name}一覧`}
+          // sectiontitleen={locale === "en" ? `${name}一覧` : `List of ${nameen}`}
+        />
+      )}
 
-      <CardA
-        emakis={filteredData}
-        columns={"four"}
-        // sectionname={"recommend"}
-        // sectiontitle={locale === "en" ? `List of ${nameen}` : `${name}一覧`}
-        // sectiontitleen={locale === "en" ? `${name}一覧` : `List of ${nameen}`}
-      />
       <Footer />
     </>
   );
