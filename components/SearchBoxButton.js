37dd7c5656e9255ce80feefd49ec03282a0cb163
satoshi-create <mimizunobouken@gmail.com>
2024-06-13 22:36:37 +0900

    ModalSearchコンポーネントを作成

--- components/SearchBoxButton.js
+++ components/SearchBoxButton.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/SearchBoxButton.module.css";
+import { AppContext } from "../pages/_app";
 
 const SearchBoxButton = () => {
+  const { openSearchModalOpen, isSearchModalOpen } = useContext(AppContext);
+  console.log(isSearchModalOpen);
   return (
-    <button className={styles.searchboxbtn}>
+    <button className={styles.searchboxbtn} onClick={openSearchModalOpen}>
       <FontAwesomeIcon icon={faMagnifyingGlass} />
       <span>...input search</span>
     </button>
-
   );
 }
 
