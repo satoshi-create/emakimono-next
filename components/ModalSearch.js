79abe5cc802e42549e785415f744b991d451ac84
satoshi-create <mimizunobouken@gmail.com>
2024-06-14 09:35:28 +0900

    ModalSearchコンポーネントにserchformを作成
    Former-commit-id: 2dbd7c5b01495e0a238869181b343419dc3037de [formerly 6228e80a88dfc12c2924ffe65fada2bbdd0b6f9e]
    Former-commit-id: 36022caec0e919fab0345e9138cd6220499816f0

--- components/ModalSearch.js
+++ components/ModalSearch.js
@@ -1,4 +1,4 @@
-import React ,{useContext}from 'react'
+import React ,{useContext,useState}from 'react'
 import { AppContext } from "../pages/_app";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,15 +8,47 @@ import styles from "../styles/Search.css.module.css";
 import CardA from './CardA';
 import ExtractingListData from "../libs/ExtractingListData";
 import { useRouter } from "next/router";
-import CardC from './CardC';
+
+
 
 const ModalSearch = () => {
+  const { openSearchModalOpen, isSearchModalOpen, closeSearchModal } =
+    useContext(AppContext);
     const { locale } = useRouter();
     const removeNestedArrayObj = ExtractingListData();
   const data = removeNestedArrayObj;
+
   
-   const { openSearchModalOpen, isSearchModalOpen, closeSearchModal } =
-     useContext(AppContext);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [showData, setShowData] = useState(data);
+
+  const handleInput = (e) => {
+      setSearchKeyword(e.currentTarget.value);
+      setShowData(filteredData);
+  };
+  
+    const regx = new RegExp(searchKeyword);
+
+    const filteredData = data.filter((item) => {
+      const title = item.title + item.edition + item.titleen;
+      const data = regx.test(title);
+      return data;
+    });
+
+    const types = Array.from(new Set(data.map((item) => item.type)));
+    console.log(types);
+
+    const selectTypes = (e) => {
+      const el = e.target.value;
+      console.log(e.target.value);
+      if (el === "作品のタイプ") {
+        setShowData(data);
+        return;
+      }
+      const selectTypeItems = data.filter((item) => item.type === el);
+      setShowData(selectTypeItems);
+    };
+  
   return (
     <div className={styles.modal}>
       <div className={styles.MuiBackdrop} onClick={closeSearchModal}></div>
@@ -24,9 +56,31 @@ const ModalSearch = () => {
         <div className={`${styles.closebtn} btn`} onClick={closeSearchModal}>
           <FontAwesomeIcon icon={faClose} />
         </div>
-        {/* <div className={styles.tabcontainer}>作品を検索する</div> */}
+        <div className={styles.tabcontainer}>
+          {/* <h4>Types</h4> */}
+          <select
+            onChange={(e) => selectTypes(e)}
+            className={styles.typeselect}
+          >
+            <option value={"作品のタイプ"}>作品のタイプ</option>
+            {types.map((type, i) => (
+              <option key={i} value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+          {/* <label htmlFor="search-keyword">Search</label> */}
+          <form>
+            <input
+              id="search-keyword"
+              type="text"
+              onInput={handleInput}
+              placeholder={"検索"}
+            />
+          </form>
+        </div>
         <div className={`${styles.contents} scrollbar`}>
-          <CardA emakis={data} columns={"searchbox"} />
+          <CardA emakis={showData} columns={"searchbox"} />
         </div>
       </div>
     </div>
