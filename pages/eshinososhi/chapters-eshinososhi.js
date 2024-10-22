c9e8dbb4b58403550b79d27751407b61013c4b36
satoshi-create <mimizunobouken@gmail.com>
2024-10-22 10:14:01 +0900

    ・chaptersEshinososhiTableコンポーネントを作成
    Former-commit-id: a7f9389a5c5fd6300d8f356bcb4c447e9192d063

--- pages/eshinososhi/chapters-eshinososhi.js
+++ pages/eshinososhi/chapters-eshinososhi.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import styles from "../../styles/ChaptersTable.module.css";
+import AllEshiChapters from "../../libs/eshi-no-soshi/eshi-no-soshi-text.json";
+import Title from "../../components/Title";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle, faXmark } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import parse from "html-react-parser";
 
-const ChaptersEsinososhi = () => {
+const chaptersEshinososhiTable = () => {
   return (
-    <div>chapters-esinososhi</div>
-  )
+    <section className={`section-grid section-padding `}>
+      <Title
+        sectiontitle={"絵師草紙諸段一覧"}
+        sectiontitleen={"List of Eshi-no-soshi"}
+      />
+      <table className={styles.table}>
+        <colgroup>
+          <col span="2" className={styles.col1} />
+          <col span="3" className={styles.col2} />
+        </colgroup>
+        <thead>
+          <tr>
+            <th>番号</th>
+            <th>段</th>
+            <th>現代文</th>
+          </tr>
+        </thead>
+        <tbody>
+          {AllEshiChapters.map((item, i) => {
+            const {chapter,title,gendaibun} = item
+            return (
+              <tr key={i}>
+                <td>{chapter}</td>
+                <td className={styles.summary}>
+                  <p>{parse(title)}</p>
+                </td>
+                <td className={styles.summary}>
+                  <p>{parse(gendaibun)}</p>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
 }
 
-export default ChaptersEsinososhi;
+export default chaptersEshinososhiTable;
