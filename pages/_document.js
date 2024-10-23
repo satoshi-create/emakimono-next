c3dd4136afc38768db9ef04defccc3882264d4ed
satoshi-create <mimizunobouken@gmail.com>
2024-10-23 11:10:27 +0900

    ・画像の読み込みを検証するために、グーグルフォントをコメントアウト
    Former-commit-id: ddb08b3b710ae3315f25584dd9aaa3bf0a7ce0d0

--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,10 @@ class MyDocument extends Document {
             rel="stylesheet"
           /> */}
           {/* Noto Sans Japaneseを追加 */}
-          <link
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Kaisei+Decol:wght@400;500;700&family=Kaisei+HarunoUmi:wght@400;500;700&family=Noto+Sans+JP:wght@300;400;500&family=Zen+Kurenaido&family=Zen+Maru+Gothic:wght@300;400;500;700&display=swap&family=Noto+Serif+JP&display=swap&family=Noto+Serif+JP&family=Zen+Kaku+Gothic+New&family=Shippori+Mincho&family=Yomogi&display=swap&family=Shippori+Mincho&display=swap&family=Hina+Mincho&family=Shippori+Mincho&display=swap&family=Caveat:wght@400..700&family=Dancing+Script:wght@400..700&family=Noto+Serif+JP:wght@200..900&family=RocknRoll+One&display=swap')"
             rel="stylesheet"
-          />
+          /> */}
         </Head>
         <body>
           <noscript
