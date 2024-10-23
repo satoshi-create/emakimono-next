0aade7ad646ba11e1d952079cf371ee2d7740f34
satoshi-create <mimizunobouken@gmail.com>
2024-10-23 12:11:59 +0900

    _document.js ・画像の読み込みを早くするために、グーグルフォントを削減
    Former-commit-id: 586a91a9461d9710900679592c6f934b2ea36ddc [formerly ad99e2f433003f0a221deb721bd34c628a97843a]
    Former-commit-id: 0f438e7468de21991420b6b2e7fda3b45c34f53c

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,19 @@ class MyDocument extends Document {
           {/* <link
             href="https://fonts.googleapis.com/css2?family=Kaisei+Decol:wght@400;500;700&family=Kaisei+HarunoUmi:wght@400;500;700&family=Noto+Sans+JP:wght@300;400;500&family=Zen+Kurenaido&family=Zen+Maru+Gothic:wght@300;400;500;700&display=swap&family=Noto+Serif+JP&display=swap&family=Noto+Serif+JP&family=Zen+Kaku+Gothic+New&family=Shippori+Mincho&family=Yomogi&display=swap&family=Shippori+Mincho&display=swap&family=Hina+Mincho&family=Shippori+Mincho&display=swap&family=Caveat:wght@400..700&family=Dancing+Script:wght@400..700&family=Noto+Serif+JP:wght@200..900&family=RocknRoll+One&display=swap')"
             rel="stylesheet"
+
           /> */}
+
+          {/* <link
+            href="https://fonts.googleapis.com/css2?family=Caveat&family=Dancing+Script&family=Hina+Mincho&family=Kaisei+Decol&family=Kaisei+HarunoUmi&family=Noto+Serif+JP&family=Shippori+Mincho&family=Yomogi&family=Zen+Kaku+Gothic+New&family=Zen+Kurenaido&family=Zen+Maru+Gothic&display=swap"
+            rel="stylesheet"
+          /> */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Hina+Mincho&family=Zen+Kaku+Gothic+New&family=Zen+Kurenaido&family=Zen+Maru+Gothic&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <noscript
