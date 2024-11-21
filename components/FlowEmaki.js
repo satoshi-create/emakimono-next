3204c047087c156a0dd6d16a4b82617934d4bf09
satoshi-create <mimizunobouken@gmail.com>
2024-11-21 23:12:46 +0900

    ・ホームページのレイアウトを変更 ・各セクションの解説を修正 ・「鳥獣人物戯画絵巻」全巻の解説を修正
    Former-commit-id: ee79cd032cd9ada6e60b720ff46cde7c66c993da [formerly 6f62614353d5e65a190ff4ae5141b9637a6cfbdc]
    Former-commit-id: fecc7809efc48fa639c633ef39120d0527431775

--- components/FlowEmaki.js
+++ components/FlowEmaki.js
@@ -10,57 +10,58 @@ import Link from "next/link";
 
 // TODO:CREATE - レスポンシブデザインを作成する
 
-const FlowEmaki = ({ flowEmakis: emakis, sectiontitle, sectiontitleen,center }) => {
+const FlowEmaki = ({ flowEmakis: emakis, sectiontitle, sectiondesc,sectiontitleen,center }) => {
   const { locale } = useRouter();
   const { orientation } = useContext(AppContext);
   return (
     <>
       <section className={`section-center section-padding`}>
         <Title sectiontitle={sectiontitle} sectiontitleen={sectiontitleen} />
+        {/* {sectiondesc && <p className={styles.sectiondesc}>{sectiondesc}</p>} */}
         {emakis.map((item, i) => {
           return (
-            <div
-              className={`${styles.container}  ${!center && (
-                i % 2 ? styles.left : styles.right
-              )
-              }`}
-              key={i}
-            >
-              <h4
-                className={`${styles.waka} ${styles.kami}`}
-                dangerouslySetInnerHTML={{
-                  __html: item.waka_kami,
-                }}
-              ></h4>
-              <h4
-                className={`${styles.waka} ${styles.simo}`}
-                dangerouslySetInnerHTML={{
-                  __html: item.waka_simo,
-                }}
-              ></h4>
-              <FullScreenComp
-                index={i}
-                edition={item.edition}
-                editionen={item.editionen}
-                titleen={item.titleen}
-                title={item.title}
-                genjieslug={item.genjieslug}
+             <div
+                className={`${styles.container}  ${
+                  !center && (i % 2 ? styles.left : styles.right)
+                }`}
+                key={i}
               >
-                <EmakiConteiner
-                  data={item}
-                  height={orientation === "portrait" ? "30vh" : "40vh"}
-                  // width={"80vw"}
-                  scroll={false}
-                  overflowX={"auto"}
-                  boxshadow={" 0 5px 15px rgba(0, 0, 0, 20%)"}
-                />
-              </FullScreenComp>
-              <div className={styles.link}>
-                <Link href={`/${item.titleen}`}>
-                  <a className={styles.linkedbutton}> 横スクロールで見る</a>
-                </Link>
+                <h4
+                  className={`${styles.waka} ${styles.kami}`}
+                  dangerouslySetInnerHTML={{
+                    __html: item.waka_kami,
+                  }}
+                ></h4>
+                <h4
+                  className={`${styles.waka} ${styles.simo}`}
+                  dangerouslySetInnerHTML={{
+                    __html: item.waka_simo,
+                  }}
+                ></h4>
+                <FullScreenComp
+                  index={i}
+                  edition={item.edition}
+                  editionen={item.editionen}
+                  titleen={item.titleen}
+                  title={item.title}
+                  desc={item.desc}
+                  genjieslug={item.genjieslug}
+                >
+                  <EmakiConteiner
+                    data={item}
+                    height={orientation === "portrait" ? "30vh" : "40vh"}
+                    // width={"80vw"}
+                    scroll={false}
+                    overflowX={"auto"}
+                    boxshadow={" 0 5px 15px rgba(0, 0, 0, 20%)"}
+                  />
+                </FullScreenComp>
+                <div className={styles.link}>
+                  <Link href={`/${item.titleen}`}>
+                    <a className={styles.linkedbutton}> 横スクロールで見る</a>
+                  </Link>
+                </div>
               </div>
-            </div>
           );
         })}
         {/* {linktitle && (
