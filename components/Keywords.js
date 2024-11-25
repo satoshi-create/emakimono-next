6f704e5f2ba10a5fd8634346ddfef3c03133c2d6
satoshi-create <mimizunobouken@gmail.com>
2024-11-25 20:26:44 +0900

    CustomTagCloud.js ・タグのtotalに応じてフォントサイズが大きくなるスタイルに変更 ・ランダムに日本の伝統色が配色されるスタイルに変更
    Former-commit-id: 969635ce73523a878f186181f10cbe0588786eac [formerly 8a9cb90c9bcad92c9de1a2be85293c45cadd4b39]
    Former-commit-id: 3ea54285a8fde459d997933b9af2284fe063bc4d

--- components/Keywords.js
+++ components/Keywords.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useMemo} from "react";
 import Title from "./Title";
 import emakisData from "../libs/data";
 import Link from "next/link";
@@ -6,28 +6,32 @@ import styles from "../styles/KeywordList.module.css";
 import { keywordItem, personnameItem } from "../libs/func";
 import { useRouter } from "next/router";
 import Button from "./Button";
+import CustomTagCloud from "../components/CustomTagCloud";
 
 const Keywords = ({ sectiontitle, sectiontitleen, path, allTags }) => {
+
+    const tags = useMemo(() => [
+    { name: 'JavaScript', count: 38 },
+    { name: 'React', count: 30 },
+    { name: 'Nodejs', count: 28 },
+    { name: 'Express.js', count: 25 },
+    { name: 'HTML5', count: 33 },
+    { name: 'MongoDB', count: 18 },
+    { name: 'CSS3', count: 20 },
+    ], []);
+
+  console.log(allTags);
+
+
+
   const { locale } = useRouter();
   return (
     <section className={`section-grid section-padding `}>
       <Title sectiontitle={sectiontitle} sectiontitleen={sectiontitleen} />
-      <div className={`${styles.tags} ${locale === "ja" && styles.jatags}`}>
-        {allTags.map((item, index) => {
-          const { name, id, slug, total, ruby } = item;
 
-          return (
-            <Link href={`/keyword/${slug}`} key={index}>
-              <a className={styles.title}>
-                <p>
-                  {locale === "en" ? id : name}
-                  <span className={styles.total}>{`(${total})`}</span>
-                </p>
-              </a>
-            </Link>
-          );
-        })}
-      </div>
+      <div className={`${styles.tags} ${locale === "ja" && styles.jatags}`}>
+        <CustomTagCloud tags={allTags} />
+         </div>
       {path && (
         <Button
           title={
