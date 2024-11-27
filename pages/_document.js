63b609593c4f426f83b9ca239055d85e50f17f01
satoshi-create <mimizunobouken@gmail.com>
2024-11-27 22:37:05 +0900

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", '${process.env.MS_CLARITY_ID}');
+              })(window, document, "clarity", "script", '${process.env.NEXT_PUBLIC_CLARITY_ID}');
             `,
             }}
           />
