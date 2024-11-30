5ef836e041866dfdb3d325c6d50924e7d42ad923
satoshi-create <mimizunobouken@gmail.com>
2024-11-30 18:39:16 +0900

    NavLinks,FullScreenComp ・フォントを調整

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Hina+Mincho&family=Zen+Kaku+Gothic+New&family=Zen+Kurenaido&family=Zen+Maru+Gothic&family=RocknRoll+One&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Hina+Mincho&family=Zen+Kaku+Gothic+New&family=Zen+Kurenaido&family=Zen+Maru+Gothic&family=RocknRoll+One&family=Noto+Sans+JP&display=swap"
             rel="stylesheet"
           />
         </Head>
