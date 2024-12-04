e47deea94137760b5b8d6847d9e9b131bf11ff3a
satoshi-create <mimizunobouken@gmail.com>
2024-12-04 22:12:25 +0900

    ・「次に進む」ボタンに常時アニメーションが表示されるよう修正 ・スマホでスマホでカルーセルの移動幅を狭くする分岐を作成
    Former-commit-id: 65e57e77d4a490538b2301b9e77c9df0ccd04fba [formerly 249d3182309641757aeae0c0a6d08c93e269d3b8]
    Former-commit-id: 9a4767d13e6a945254cfb50371099837a85c3dbc

--- components/CarouselButton.js
+++ components/CarouselButton.js
@@ -1,4 +1,4 @@
-import React, { useRef,useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, useContext } from "react";
 import { Box, Button, Flex, IconButton } from "@chakra-ui/react";
 import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 import ActionButton from "./ActionButton";
@@ -11,24 +11,27 @@ import {
   faCircleQuestion,
 } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
+import { AppContext } from "../pages/_app";
 
 const CarouselComponent = ({ scrollNextRef, scrollPrevRef, articleRef }) => {
+const { orientation } = useContext(AppContext);
+
   const con = articleRef.current;
 
   const [nextClicked, setNextClicked] = useState(false);
-  const [highlightNext, setHighlightNext] = useState(false); // 強調するフラグ
+  const [highlightNext, setHighlightNext] = useState(true); // 強調するフラグ
 
   // 初回のレンダリング時に次に進むボタンを強調
-  useEffect(() => {
-    setHighlightNext(true);
+  // useEffect(() => {
+  //   setHighlightNext(true);
 
-    // アニメーションが終わったらフラグをfalseに戻す
-    const timer = setTimeout(() => {
-      setHighlightNext(false);
-    }, 10000); // 2秒後に強調を解除
+  //   // アニメーションが終わったらフラグをfalseに戻す
+  //   const timer = setTimeout(() => {
+  //     setHighlightNext(false);
+  //   }, 10000); // 2秒後に強調を解除
 
-    return () => clearTimeout(timer); // クリーンアップ
-  }, []);
+  //   return () => clearTimeout(timer); // クリーンアップ
+  // }, []);
 
   console.log(nextClicked);
 
@@ -38,11 +41,12 @@ const CarouselComponent = ({ scrollNextRef, scrollPrevRef, articleRef }) => {
     if (con) {
       // 横スクロール（右方向）
       con.scrollTo({
-        left: con.scrollLeft - 1000,
+        left: orientation === "landscape" ? con.scrollLeft - 1000 :con.scrollLeft - 300,
         behavior: "smooth",
       });
     }
   };
+
 
   // 元に戻るボタンがクリックされたときの処理
   const handlePrevClick = () => {
