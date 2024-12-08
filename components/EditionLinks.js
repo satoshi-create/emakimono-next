544250f6b167b3820dd5e8a396d9879c344b91b3
satoshi-create <mimizunobouken@gmail.com>
2024-12-08 11:47:55 +0900

    EmakiLandscapContent ・metadataBに「他の巻を見る」の項目を作成 ・metadataBにタイトルを追加
    Former-commit-id: af9aad9634c79403c11f9a88a55d320c6358dae2 [formerly 7a294be2e3f004f9feab1024ce3ceec7549347be]
    Former-commit-id: 6446739f6e81203123ffa89f2084e35a1d4c32e1

--- components/EditionLinks.js
+++ components/EditionLinks.js
@@ -1,60 +1,26 @@
 import React from "react";
-import { useLocaleData} from "../libs/func";
+import { useLocaleData } from "../libs/func";
 import { useRouter } from "next/router";
 import { ExternalLink } from "react-feather";
 import Link from "next/link";
 import styles from "../styles/EditionLinks.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faScroll
-} from "@fortawesome/free-solid-svg-icons";
+import { faScroll } from "@fortawesome/free-solid-svg-icons";
+import SingleCardA from "./SingleCardA";
 
-const EditionLinks = ({ title, edition }) => {
-  const { locale } = useRouter();
-  const { t: alldata } = useLocaleData();
-  const editionLinks = alldata.filter(
-    (item) => item.title === title && item.edition !== edition
-  );
+const EditionLinks = ({ title, edition,editionLinks }) => {
+  // const { locale } = useRouter();
+  // const { t: alldata } = useLocaleData();
+  // const editionLinks = alldata.filter(
+  //   (item) => item.title === title && item.edition !== edition
+  // );
+
   return (
-    <>
-      {editionLinks.length > 0 && (
-        <ul className={styles.editionLink}>
-          {editionLinks.map((item, i) => {
-            return (
-              <li key={i}>
-                <Link href={`./${item.titleen}`}>
-                  <a target="_blank">
-                    {locale == "en" ? (
-                      <p>
-                        <FontAwesomeIcon
-                          icon={faScroll}
-                          className={styles.scrollIcon}
-                        />
-                        {item.titleen}
-                        {item.edition !== edition && (
-                          <ExternalLink className={styles.ExternalLinkIcon} />
-                        )}
-                      </p>
-                    ) : (
-                      <p>
-                        <FontAwesomeIcon
-                          icon={faScroll}
-                          className={styles.scrollIcon}
-                        />
-                        {item.title} {item.edition}
-                        {/* {item.edition !== edition && (
-                          <ExternalLink className={styles.ExternalLinkIcon} />
-                        )} */}
-                      </p>
-                    )}
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </>
+    <div className={styles.four}>
+      {editionLinks.map((item, i) => {
+        return <SingleCardA item={item} key={i} variant={"editionlink"} />;
+      })}
+    </div>
   );
 };
 
