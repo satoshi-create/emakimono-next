a84b5e6c79fd4fcb11de3d335bbcc156e24152ae
satoshi-create <mimizunobouken@gmail.com>
2024-12-11 11:26:28 +0900

    next.config.jsにサービスワーカーの登録を有効にする設定を追加
    Former-commit-id: 282785f773151cda7bda449a8d5ac9c7e0609151

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
+    dest: 'public',  // PWAのリソースを出力する場所
+  register: true,  // サービスワーカーの登録を有効にする
+  skipWaiting: true,  // 新しいサービスワーカーがインストールされたときにページを即座にリロード
+  disable: process.env.NODE_ENV === 'development',
 })
 
 const nextConfig = {
