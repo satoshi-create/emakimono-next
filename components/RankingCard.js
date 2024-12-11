c7d4a3408b56006e8c6084d18d94e32336cee8f9
satoshi-create <mimizunobouken@gmail.com>
2024-12-11 13:20:02 +0900

    fix
    Former-commit-id: 8544432aafb65ecbb55a7c2db55f43894264bb75

--- components/RankingCard.js
+++ components/RankingCard.js
@@ -41,117 +41,108 @@ export default function RankingCard({ isCompact = false }) {
         rowGap={isCompact ? "2" : "10"}
       >
         {data(isCompact).map((item, i) => (
-          <Link href={`/${item.titleen}`} key={i} >
-            <ChakraLink
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
-              flex="1"
-              isExternal
-            >
-              <Box
-                borderWidth={1}
-                borderRadius="lg"
-                overflow="hidden"
-                transition="all 0.3s"
-                _hover={{ boxShadow: "lg" }}
+          <Link href={`/${item.titleen}`} key={i}>
+            <a>
+              <ChakraLink
+                textDecoration="none"
+                _hover={{ textDecoration: "none" }}
+                flex="1"
+                isExternal
               >
-                <Flex
-                  direction={isCompact ? "row" : { base: "column", sm: "row" }}
+                <Box
+                  borderWidth={1}
+                  borderRadius="lg"
+                  overflow="hidden"
+                  transition="all 0.3s"
+                  _hover={{ boxShadow: "lg" }}
                 >
-                  <Box
-                    position="relative"
-                    width={isCompact ? "150px" : { base: "full", sm: "60%" }}
-                    // height={isCompact ? "100px" : "300px"}
+                  <Flex
+                    direction={
+                      isCompact ? "row" : { base: "column", sm: "row" }
+                    }
                   >
-                    <Badge
-                      position="absolute"
-                      top={2}
-                      left={2}
-                      zIndex={1}
-                      colorScheme={
-                        i + 1 === 1 ? "red" : i + 1 === 2 ? "orange" : "yellow"
-                      }
-                      fontSize={isCompact ? "sm" : "lg"}
-                      fontWeight="bold"
-                      paddingX={isCompact ? 1 : 3}
-                      paddingY={isCompact ? 0.5 : 1}
+                    <Box
+                      position="relative"
+                      width={isCompact ? "150px" : { base: "full", sm: "60%" }}
+                      // height={isCompact ? "100px" : "300px"}
                     >
-                      {i + 1}位
-                    </Badge>
-                    <Image
-                      src={item.thumb}
-                      alt={item.thumb}
-                      objectFit="cover"
-                      width="100%"
-                      height="auto"
-                    />
-                  </Box>
-                  <Box p={isCompact ? 2 : 4} flex={1}>
-                    <Flex
-                      justifyContent="space-between"
-                      alignItems="flex-start"
-                    >
-                      <VStack align="start" spacing={1}>
-                        <HStack>
-                          <Badge variant="outline">{item.type}</Badge>
-                          <Badge variant="outline">{item.era}時代</Badge>
-                        </HStack>
-                        <Text
-                          fontSize={isCompact ? "md" : "xl"}
-                          fontWeight="bold"
-                        >
-                          {item.title} {item.edition ? item.edition : ""}
-                        </Text>
-                      </VStack>
-                    </Flex>
-                    <HStack
-                      marginTop={isCompact ? 2 : 4}
-                      color="gray.500"
-                      fontSize={isCompact ? "sm" : "md"}
-                    >
-                      <ViewIcon />
-                      <Text>{item.pageView.toLocaleString()}回鑑賞</Text>
-                    </HStack>
-                    {/* {i + 1 === 1 && (
+                      <Badge
+                        position="absolute"
+                        top={2}
+                        left={2}
+                        zIndex={1}
+                        colorScheme={
+                          i + 1 === 1
+                            ? "red"
+                            : i + 1 === 2
+                            ? "orange"
+                            : "yellow"
+                        }
+                        fontSize={isCompact ? "sm" : "lg"}
+                        fontWeight="bold"
+                        paddingX={isCompact ? 1 : 3}
+                        paddingY={isCompact ? 0.5 : 1}
+                      >
+                        {i + 1}位
+                      </Badge>
+                      <Image
+                        src={item.thumb}
+                        alt={item.thumb}
+                        objectFit="cover"
+                        width="100%"
+                        height="auto"
+                      />
+                    </Box>
+                    <Box p={isCompact ? 2 : 4} flex={1}>
+                      <Flex
+                        justifyContent="space-between"
+                        alignItems="flex-start"
+                      >
+                        <VStack align="start" spacing={1}>
+                          <HStack>
+                            <Badge variant="outline">{item.type}</Badge>
+                            <Badge variant="outline">{item.era}時代</Badge>
+                          </HStack>
+                          <Text
+                            fontSize={isCompact ? "md" : "xl"}
+                            fontWeight="bold"
+                          >
+                            {item.title} {item.edition ? item.edition : ""}
+                          </Text>
+                        </VStack>
+                      </Flex>
                       <HStack
                         marginTop={isCompact ? 2 : 4}
-                        color="orange.500"
+                        color="gray.500"
                         fontSize={isCompact ? "sm" : "md"}
                       >
-                        <ExternalLinkIcon
-                          display={isCompact ? "none" : "block"}
-                        />
-                        <Text
-                          fontWeight="medium"
-                          display={isCompact ? "none" : "block"}
-                        >
-                          今月の人気コンテンツ
-                        </Text>
+                        <ViewIcon />
+                        <Text>{item.pageView.toLocaleString()}回鑑賞</Text>
                       </HStack>
-                    )} */}
-                  </Box>
-                </Flex>
-              </Box>
-            </ChakraLink>
+                        </Box>
+                  </Flex>
+                </Box>
+              </ChakraLink>
+            </a>
           </Link>
         ))}
       </Stack>
       {isCompact && (
-          <Box marginTop={6} textAlign="right">
-            <Link href="/ranking">
-              <a>
-                <Button
-                  as={ChakraLink}
-                  rightIcon={<ArrowForwardIcon />}
-                  borderRadius="md"
-                  borderWidth="1px"
-                  fontSize="small"
-                >
-                  全ての絵巻ランキングを見る
-                </Button>
-              </a>
-            </Link>
-          </Box>
+        <Box marginTop={6} textAlign="right">
+          <Link href="/ranking">
+            <a>
+              <Button
+                as={ChakraLink}
+                rightIcon={<ArrowForwardIcon />}
+                borderRadius="md"
+                borderWidth="1px"
+                fontSize="small"
+              >
+                全ての絵巻ランキングを見る
+              </Button>
+            </a>
+          </Link>
+        </Box>
       )}
     </Box>
   );
