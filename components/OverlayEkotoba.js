74a3700969af5a46f9bda30f845661689323eaa7
satoshi-create <mimizunobouken@gmail.com>
2024-12-19 21:52:46 +0900

    Cloudinaryが有効な場合のみローダー適用する分岐を作成
    Former-commit-id: 1b339feaedd4a1387d219f0ecb7f9113a359924d [formerly e2f4dc2f214df7b7fca35393f2083ce6fa3f4f38]
    Former-commit-id: 62162d684f7700dc335454e6b3815e124beee767

--- components/OverlayEkotoba.js
+++ components/OverlayEkotoba.js
@@ -14,6 +14,7 @@ import parse from "html-react-parser";
 import "lazysizes";
 import "lazysizes/plugins/attrchange/ls.attrchange";
 import ActionButton from "./ActionButton";
+import LazyImage from "./LazyImage";
 
 const OverlayEkotoba = ({
   item: {
@@ -23,6 +24,7 @@ const OverlayEkotoba = ({
     srcSp,
     srcTb,
     src,
+    config,
     load,
     name,
     scroll,
@@ -39,6 +41,7 @@ const OverlayEkotoba = ({
     data,
     googlemap,
   },
+  item,
 }) => {
   const {
     setekotobaToggle,
