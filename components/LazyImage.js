6bfa3e11c3caa9add29d03df5b2fa0d783cb0400
satoshi-create <mimizunobouken@gmail.com>
2025-01-06 21:18:51 +0900

    fix
    Former-commit-id: 9c05183331537a9948a86fb7dc24977511969504

--- components/LazyImage.js
+++ components/LazyImage.js
@@ -10,16 +10,12 @@ const LazyImage = ({
   width,
   height,
   srcSp,
-  srcTb,
   config,
   isBlurVisible,
   uniqueKey,
   index,
   uniqueIndex,
 }) => {
-  const [isSkeletonVisible, setSkeletonVisible] = useState(true); // スケルトンの表示状態
-  // const [isVisible, setIsVisible] = useState(false);
-  const [isLoaded, setIsLoaded] = useState(false);
   const { windowHeight, orientation, toggleFullscreen } =
     useContext(AppContext);
 
@@ -44,47 +40,43 @@ const LazyImage = ({
   }, []);
 
   const baseUrl =
-    "https://res.cloudinary.com/dw2gjxrrf/image/upload/fl_progressive,f_jpg";
+    "https://res.cloudinary.com/dw2gjxrrf/image/upload/fl_progressive";
 
   // 高さに基づいて適切な画像ソースを選択
-  const getResponsiveSrc = () => {
-    if (windowHeight <= 800) {
-      return src.srcTb; // タブレット用
+  const getResponsiveSrc = (emaki) => {
+    if (windowHeight <= 375) {
+      return emaki.srcSp; // スマートフォン用
+    } else if (windowHeight <= 800) {
+      return emaki.srcTb; // タブレット用
     } else {
-      return src.src; // デスクトップ用
+      return emaki.src; // デスクトップ用
     }
   };
 
   const cloudinaryLoader = ({ src, width, quality }) => {
-    return `https://res.cloudinary.com/dw2gjxrrf/image/upload/fl_progressive,f_jpg,w_${width},q_${
-      quality || 75
-    }/${src}`;
+    return `${baseUrl},f_jpg,w_${width},q_${quality || 75}/${src}`;
   };
 
-  const getResponsiveSrcCloudinary = (emaki) => {
+  const getResponsiveSrcCloudinary = (emaki, containerHeight) => {
     const aspectRatio = width / height; // アスペクト比を計算
 
-    // デバイスの高さに応じてCloudinaryの画像サイズを動的に調整
-    if (windowHeight <= 375) {
+    // コンテナの高さに応じてCloudinaryの画像サイズを動的に調整
+    if (containerHeight <= 375) {
       const calculatedWidth = Math.round(375 * aspectRatio); // 高さから幅を計算
       return `${baseUrl}/w_${calculatedWidth},h_375,c_fit/${emaki.src}`; // スマートフォン用
-    } else if (windowHeight <= 800) {
+    } else if (containerHeight <= 800) {
       const calculatedWidth = Math.round(800 * aspectRatio); // 高さから幅を計算
-      return `${baseUrl}/w_${calculatedWidth},h_800,q_${
-        quality || 75
-      },c_fit/${src}`; // スマホ・タブレット用
+      return `${baseUrl}/w_${calculatedWidth},h_800,c_fit/${emaki.src}`; // タブレット用
     } else {
-      return `${baseUrl}/w_${width},h_${height},c_fit/${emaki.src}`; // デスクトップ用
+      const calculatedWidth = Math.round(containerHeight * aspectRatio); // 高さから幅を計算
+      return `${baseUrl}/w_${calculatedWidth},h_${containerHeight},c_fit/${emaki.src}`; // デスクトップ用
     }
   };
 
+  const responsiveSrc = getResponsiveSrcCloudinary(src, containerHeight);
+
   // 低解像度画像（ぼかしプレースホルダー用）
-  // const blurImage = `${baseUrl}/w_10,h_10,c_fill,q_auto:low/${src.src}`;
-
-  const aspectRatio = width / height; // アスペクト比を計算
-  const calculatedWidth = Math.round(375 * aspectRatio); // 高さから幅を計算
-
-  const blurImage = `${baseUrl}/w_${calculatedWidth},h_375,c_fit/${src.src}`; // スマートフォン用
+  const blurImage = `${baseUrl}/w_10,h_10,c_fill,q_auto:low/${src.src}`;
 
   // 少し大きめの低解像度画像（ぼかしプレースホルダー用）
   // const blurImage = `${baseUrl}/w_50,h_50,c_fill,q_auto:low/${src.src}`;
@@ -127,13 +119,12 @@ const LazyImage = ({
       {isSkeletonVisible && <div className="skeleton"></div>}
       {isBlurVisible && (
         <Image
-          loader={config === "cloudinary" ? cloudinaryLoader : getResponsiveSrc} // Cloudinaryが有効な場合のみローダー適用
-          src={src.src}
+          loader={config === "cloudinary" ? cloudinaryLoader : undefined} // Cloudinaryが有効な場合のみローダー適用
+          src={src.src} // Cloudinaryの画像ID
           width={width}
           height={height}
           alt={alt}
           priority={uniqueIndex === 0} // 最初の画像は即時プリロード
-          // loading={"lazy"} // 最初の2枚は即時読み込み
           loading={uniqueIndex < 2 ? "eager" : "lazy"} // 最初の2枚は即時読み込み
           layout="responsive"
           placeholder={"blur"} // 最初の2枚だけぼかしプレースホルダーを適用
