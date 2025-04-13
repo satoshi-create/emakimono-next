789ed73bc4caeb5aa543cb18febf73adee736166
satoshi-create <mimizunobouken@gmail.com>
2025-04-13 15:19:15 +0900

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,17 +4,11 @@ import Head from "@/components/meta/Meta";
 import Breadcrumbs from "@/components/navigation/Breadcrumbs";
 import Button from "@/components/ui/Button";
 import Title from "@/components/ui/Title";
-import { useLocale, useLocaleData } from "@/utils/func";
 import "lazysizes";
 import { useRouter } from "next/router";
 
 const Custom404 = () => {
   const { locale } = useRouter();
-  const { t } = useLocale();
-
-  const { t: data } = useLocaleData();
-
-  const chinkaEmakis = data.filter((emaki) => emaki.title === "鎮火安心図巻");
   return (
     <>
       <Head />
@@ -28,12 +22,6 @@ const Custom404 = () => {
           sectiontitleen={"PAGE NOT FOUND"}
         />
         <Button title="ホームに戻る" path="/" />
-        {/* <FlowEmaki
-          flowEmakis={chinkaEmakis}
-          // sectiontitle={"四季山水図巻（山水長巻）"}
-          // sectiontitleen={"sessyu_sikisansuizu"}
-          center={true}
-        /> */}
       </section>
       <Footer />
     </>
